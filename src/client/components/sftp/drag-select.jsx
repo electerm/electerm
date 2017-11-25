99f5b3c77b8f33a2642db4f925dfe91f2c80d3ef
ZHAO Xudong <zxdong@gmail.com>
2017-11-25 16:22:15 +0800

    save

---
+++ src/client/components/sftp/drag-select.jsx
@@ -0,0 +1,85 @@
+/**
+ * drag to select file/folder
+ */
+
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class Confirms extends React.Component {
+
+  static propTypes = {
+    className: PropTypes.string,
+    wrapperSelector: PropTypes.string,
+    targetSelector: PropTypes.string,
+    style: PropTypes.object
+  }
+
+  static defaultProps = {
+    className: 'drag-select-mask',
+    targetSelector: '.sftp-item',
+    wrapperSelector: '.virtual-file',
+    style: {}
+  }
+
+  state = {
+    maskStyle: {
+      width: 0,
+      height: 0,
+      left: 0,
+      top: 0
+    },
+    onDrag: false,
+    endPoint: null,
+    startPoint: null
+  }
+
+  componentDidMount() {
+    this.initEvents()
+  }
+
+  componentWillUnmount() {
+    this.destroyEvents()
+  }
+
+  initEvents() {
+    let root = document.querySelector(this.props.wrapperSelector)
+    if (!root) {
+      return
+    }
+    this.root = root
+    document.addEventListener('mousedown', this.onMousedown)
+    document.addEventListener('mouseup', this.onMouseup)
+  }
+
+  onMousedown = e => {
+    this.setState({
+      onDrag: true
+    })
+  }
+
+  onMouseup = e => {
+
+  }
+
+  render() {
+    let {style, className} = this.props
+    const styles = {
+      position: 'absolute',
+      background: 'rgba(199, 207, 254, 0.3)',
+      border: 'solid 1px rgba(155, 155, 155, 0.56)',
+      zIndex: 90,
+      cursor: 'crosshair',
+      ...this.state.maskStyle,
+      ...style
+    }
+    if (!this.state.onDrag || !this.state.endPoint || !this.state.startPoint) {
+      return null
+    }
+    return (
+      <div className={className} style={styles} />
+    )
+  }
+
+}
+
