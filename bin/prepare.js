3abc72824ce7d022b453be5fb39e937d40daa4b6
ZHAO Xudong <zxdong@gmail.com>
2017-11-29 11:57:36 +0800

    fix

--- bin/prepare.js
+++ bin/prepare.js
@@ -29,9 +29,7 @@ rm('-rf',  'work/app/user-config.json')
 rm('-rf',  'work/app/localstorage.json')
 rm('-rf',  'work/app/assets/js/common-css.bundle.js')
 
-exec(`cd work/app && npm prune --production && \\
- ../../node_modules/.bin/n-prune && \\
- cd ${cwd}`)
+exec(`cd work/app && npm prune --production && ../../node_modules/.bin/n-prune && cd ${cwd}`)
 
 const endTime = +new Date()
 echo(`done pack prepare in ${(endTime - timeStart)/1000} s`)
