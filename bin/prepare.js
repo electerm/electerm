8c669babd16063ea7f4f3066891dc83c289239ea
ZHAO Xudong <zxdong@gmail.com>
2017-11-29 09:44:06 +0800

    save

---
+++ bin/prepare.js
@@ -0,0 +1,37 @@
+/**
+ * prepare the files to be packed
+ */
+
+const {version} = require('../package.json')
+const {mkdir, rm, exec, echo, cp} = require('shelljs')
+const dir = 'dist/v' + version
+const cwd = process.cwd()
+
+echo('start pack prepare')
+
+const timeStart = + new Date()
+
+rm('-rf', dir)
+rm('-rf', 'dist/latest')
+rm('-rf', 'work')
+
+mkdir('-p', dir)
+mkdir('-p', 'dist/latest')
+mkdir('-p', 'work')
+cp('-r', 'app', 'work/')
+cp('-r', [
+  'package.json',
+  'node_modules',
+  'version'
+], 'work/app/')
+rm('-rf',  'work/app/config.js')
+rm('-rf',  'work/app/user-config.json')
+rm('-rf',  'work/app/localstorage.json')
+rm('-rf',  'work/app/assets/js/common-css.bundle.js')
+
+exec(`cd work/app && npm prune --production && \\
+ ../../node_modules/.bin/n-prune && \\
+ cd ${cwd}`)
+
+const endTime = +new Date()
+echo(`done pack prepare in ${(endTime - timeStart)/1000} s`)
