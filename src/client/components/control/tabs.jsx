9c133e383044267efa2ac2ebb9aef20e1882d3ed
ZHAO Xudong <zxdong@gmail.com>
2018-01-04 10:13:29 +0800

    fix #167 UI adjust & tabs UI fix (#179)

--- src/client/components/control/tabs.jsx
+++ src/client/components/control/tabs.jsx
@@ -3,40 +3,166 @@
  * @param {array} props.tabs {id, title}
  */
 
-import {Icon, Tooltip} from 'antd'
+import React from 'react'
+import _ from 'lodash'
+import {Icon, Button, Dropdown, Menu} from 'antd'
 import Tab from './tab'
 import './tabs.styl'
+import {tabWidth, tabMargin} from '../../common/constants'
+import createName from '../../common/create-title'
 
+const ButtonGroup = Button.Group
+const MenuItem = Menu.Item
+const extraWidth = 113
 //todo: scroll controll and keyboard controll
 //todo: drag and drop
 
-export default function Tabs(props) {
 
-  let {tabs = [], onAdd} = props
-  return (
-    <div className="tabs">
-      <div className="tabs-bg" onDoubleClick={onAdd} />
-      <div className="tabs-inner">
+
+export default class Tabs extends React.Component {
+
+  componentDidMount() {
+    this.dom = document.querySelector('.tabs-inner')
+  }
+
+  componentDidUpdate() {
+    this.adjustScroll()
+  }
+
+  adjustScroll = () => {
+    let {width, tabs, currentTabId} = this.props
+    let index = _.findIndex(tabs, t => t.id === currentTabId)
+    let w = (index + 1) * (tabMargin + tabWidth) + 5
+    let scrollLeft = w > width - extraWidth
+      ? w - width + extraWidth
+      : 0
+    this.dom.scrollLeft = scrollLeft
+  }
+
+  scrollLeft = () => {
+    let {scrollLeft} = this.dom
+    scrollLeft = scrollLeft - tabMargin - tabWidth
+    if (scrollLeft < 0) {
+      scrollLeft = 0
+    }
+    this.dom.scrollLeft = scrollLeft
+  }
+
+  scrollRight = () => {
+    let {scrollLeft} = this.dom
+    scrollLeft = scrollLeft + tabMargin + tabWidth
+    if (scrollLeft < 0) {
+      scrollLeft = 0
+    }
+    this.dom.scrollLeft = scrollLeft
+  }
+
+  onClickMenu = ({key}) => {
+    let id = key.split('##')[1]
+    this.props.onChange(id)
+  }
+
+  renderList = () => {
+    let {tabs = []} = this.props
+    return (
+      <Menu onClick={this.onClickMenu}>
         {
-          tabs.map((tab, i) => {
+          tabs.map((t, i) => {
             return (
-              <Tab
-                {...props}
-                tab={tab}
-                key={i + 'tab'}
-              />
+              <MenuItem
+                key={i + '##' + t.id}
+              >{createName(t)}
+              </MenuItem>
             )
           })
         }
+      </Menu>
+    )
+  }
+
+  renderAddBtn = () => {
+    return (
+      <Icon
+        type="plus-circle-o"
+        title="open new terminal"
+        className="pointer tabs-add-btn font16"
+        onClick={this.props.onAdd}
+      />
+    )
+  }
+
+  renderExtra() {
+    return (
+      <div className="tabs-extra pd1x">
+        {this.renderAddBtn()}
+        <ButtonGroup className="iblock mg1x">
+          <Button
+            icon="left"
+            onClick={this.scrollLeft}
+          />
+          <Button
+            icon="right"
+            onClick={this.scrollRight}
+          />
+        </ButtonGroup>
+        <Dropdown
+          className="iblock"
+          placement="bottomRight"
+          overlay={this.renderList()}
+        >
+          <Icon type="down" />
+        </Dropdown>
       </div>
-      <Tooltip title="open new terminal" placement="rightTop">
-        <Icon
-          type="plus-circle-o"
-          className="pointer tabs-add-btn font16"
-          onClick={onAdd}
-        />
-      </Tooltip>
-    </div>
-  )
+    )
+  }
+
+  render() {
+    let {tabs = [], onAdd, width} = this.props
+    let len = tabs.length
+    let addBtnWidth = 22
+    let tabsWidthAll = (tabMargin + tabWidth) * len + 10
+    let overflow = width < (tabsWidthAll + addBtnWidth)
+    //let extraw = overflow ? extraWidth : 0
+    return (
+      <div className="tabs">
+        <div className="tabs-bg" onDoubleClick={onAdd} />
+        <div
+          className="tabs-inner"
+          style={{
+            width
+          }}
+        >
+          <div
+            className="tabs-wrapper"
+            style={{
+              width: tabsWidthAll + extraWidth + 10
+            }}
+          >
+            {
+              tabs.map((tab, i) => {
+                return (
+                  <Tab
+                    {...this.props}
+                    tab={tab}
+                    key={i + 'tab'}
+                  />
+                )
+              })
+            }
+            {
+              !overflow
+                ? this.renderAddBtn()
+                : null
+            }
+          </div>
+        </div>
+        {
+          overflow
+            ? this.renderExtra()
+            : null
+        }
+      </div>
+    )
+  }
 
 }
