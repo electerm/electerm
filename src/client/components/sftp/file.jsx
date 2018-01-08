49f81f380dfee0c0ca45673b17d5f09b71893dcf
ZHAO Xudong <zxdong@gmail.com>
2018-01-08 15:29:22 +0800

    fix #195 add more test (#204)

--- src/client/components/sftp/file.jsx
+++ src/client/components/sftp/file.jsx
@@ -743,7 +743,7 @@ export default class FileSection extends React.Component {
       : 'right'
     let title = (
       <div>
-        <p>{name}</p>
+        <p className="wordbreak">{name}</p>
         <p>modifyTime: {moment(modifyTime).format()}</p>
       </div>
     )
