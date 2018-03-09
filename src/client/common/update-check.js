f7d8d716c05ee46f6b2e14e4a117d73300d60cc8
ZHAO Xudong <zxdong@gmail.com>
2018-03-08 20:39:17 -0600

    fix #339 fix update version check (#341)

--- src/client/common/update-check.js
+++ src/client/common/update-check.js
@@ -5,7 +5,7 @@
 import fetch from './fetch'
 
 export async function getLatestReleaseInfo() {
-  let url = 'http://electerm.html5beta.com/version'
+  let url = 'https://electerm.html5beta.com/version.html'
   let tag_name = await fetch.get(url, null, {
     headers: {
       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
