d4d185c0438b1c3e486bd2e236b80e30cdbb5df2
ZHAO Xudong <zxdong@gmail.com>
2018-04-20 17:15:06 +0800

    save

--- src/client/components/common/resize-wrap.jsx
+++ src/client/components/common/resize-wrap.jsx
@@ -56,28 +56,6 @@ export default class ResizeWrap extends Component {
 
   }
 
-  onDropFile = (fromFile, toFile) => {
-    let {type: fromType} = fromFile
-    let {
-      type: toType,
-      isDirectory: isDirectoryTo
-    } = toFile
-
-    //same side and drop to file, do nothing
-    if (fromType === toType && !isDirectoryTo) {
-      return
-    }
-
-    //same side and drop to folder, do mv
-    if (fromType === toType && isDirectoryTo) {
-      return this.mv(fromFile, toFile)
-    }
-
-    //other side, do transfer
-    this.transferDrop(fromFile, toFile)
-
-  }
-
   buildHandleComponent = (prevComponent, direction, index) => {
     let cls = classnames(
       'term-dragger',
