068c83de0b48814e22ce23b0b66d07b49f6e59ca
ZHAO Xudong <zxdong@gmail.com>
2018-04-27 15:34:52 +0800

    fix

--- src/client/components/terminal/terminal.jsx
+++ src/client/components/terminal/terminal.jsx
@@ -574,6 +574,7 @@ export default class Term extends React.Component {
             }}
           />
         </div>
+        <Spin className="loading-wrapper" spinning={loading} />
       </div>
     )
   }
