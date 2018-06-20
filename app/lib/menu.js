0b3fbe0600b9482f9353eb4fef6633d82091f83c
ZHAO Xudong <zxdong@gmail.com>
2018-06-20 09:51:41 +0800

    fix #516 add restart menu (#517)

--- app/lib/menu.js
+++ app/lib/menu.js
@@ -1,11 +1,19 @@
 /**
  * app system menu config
  */
-const {app, Menu, shell} = require('electron')
-let {NODE_ENV} = process.env
+const {
+  app,
+  Menu,
+  shell
+} = require('electron')
+let {
+  NODE_ENV
+} = process.env
 const isDev = NODE_ENV === 'development'
 const packInfo = require(isDev ? '../../package.json' : '../package.json')
-const {prefix} = require('./locales')
+const {
+  prefix
+} = require('./locales')
 const e = prefix('menu')
 const c = prefix('control')
 
@@ -15,47 +23,111 @@ const template = [
     submenu: [
       {
         label: c('newSsh'),
-        click () {
+        click() {
           require('./win').win.webContents.send('new-ssh', null)
         }
       },
-      {role: 'undo', label: e('undo')},
-      {role: 'redo', label: e('redo')},
-      {type: 'separator'},
-      {role: 'cut', label: e('cut')},
-      {role: 'copy', label: e('copy')},
-      {role: 'paste', label: e('paste')},
-      {role: 'pasteandmatchstyle', label: e('pasteandmatchstyle')},
-      {role: 'delete', label: e('del')},
-      {role: 'selectall', label: e('selectall')}
+      {
+        role: 'undo',
+        label: e('undo')
+      },
+      {
+        role: 'redo',
+        label: e('redo')
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'cut',
+        label: e('cut')
+      },
+      {
+        role: 'copy',
+        label: e('copy')
+      },
+      {
+        role: 'paste',
+        label: e('paste')
+      },
+      {
+        role: 'pasteandmatchstyle',
+        label: e('pasteandmatchstyle')
+      },
+      {
+        role: 'delete',
+        label: e('del')
+      },
+      {
+        role: 'selectall',
+        label: e('selectall')
+      }
     ]
   },
   {
     label: e('view'),
     submenu: [
-      {role: 'reload', label: e('reload')},
-      {role: 'forcereload', label: e('forcereload')},
-      {role: 'toggledevtools', label: e('toggledevtools')},
+      {
+        role: 'reload',
+        label: e('reload')
+      },
+      {
+        role: 'forcereload',
+        label: e('forcereload')
+      },
+      {
+        role: 'toggledevtools',
+        label: e('toggledevtools')
+      },
       {
         label: e('toggleControl'),
-        click () {
+        click() {
           require('./win').win.webContents.send('toggle-control', null)
         }
       },
-      {type: 'separator'},
-      {role: 'resetzoom', label: e('resetzoom')},
-      {role: 'zoomin', label: e('zoomin')},
-      {role: 'zoomout', label: e('zoomout')},
-      {type: 'separator'},
-      {role: 'togglefullscreen', label: e('togglefullscreen')}
+      {
+        type: 'separator'
+      },
+      {
+        role: 'resetzoom',
+        label: e('resetzoom')
+      },
+      {
+        role: 'zoomin',
+        label: e('zoomin')
+      },
+      {
+        role: 'zoomout',
+        label: e('zoomout')
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'togglefullscreen',
+        label: e('togglefullscreen')
+      }
     ]
   },
   {
     role: 'window',
     label: e('window'),
     submenu: [
-      {role: 'minimize', label: e('minimize')},
-      {role: 'close', label: e('close')}
+      {
+        role: 'minimize',
+        label: e('minimize')
+      },
+      {
+        role: 'close',
+        label: e('close')
+      },
+      {
+        label: e('restart'),
+        click() {
+          require('./win').win.close()
+          app.relaunch()
+        }
+      }
     ]
   },
   {
@@ -64,7 +136,7 @@ const template = [
     submenu: [
       {
         label: e('about'),
-        click () {
+        click() {
           require('./win').win.webContents.send('open-about', null)
         }
       },
@@ -76,21 +148,21 @@ const template = [
       },
       {
         label: e('reportIssue'),
-        click () {
+        click() {
           shell
             .openExternal('https://github.com/electerm/electerm/issues/new')
         }
       },
       {
         label: 'github',
-        click () {
+        click() {
           shell
             .openExternal('https://github.com/electerm/electerm')
         }
       },
       {
         label: e('homepage'),
-        click () {
+        click() {
           shell
             .openExternal(packInfo.homepage)
         }
@@ -109,35 +181,73 @@ if (process.platform === 'darwin') {
   template.unshift({
     label: app.getName(),
     submenu: [
-      {role: 'services', submenu: []},
-      {type: 'separator'},
-      {role: 'hide', label: e('hide')},
-      {role: 'hideothers', label: e('hideothers')},
-      {role: 'unhide', label: e('unhide')},
-      {type: 'separator'},
-      {role: 'quit', label: e('quit')}
+      {
+        role: 'services',
+        submenu: []
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'hide',
+        label: e('hide')
+      },
+      {
+        role: 'hideothers',
+        label: e('hideothers')
+      },
+      {
+        role: 'unhide',
+        label: e('unhide')
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit',
+        label: e('quit')
+      }
     ]
   })
 
   // Edit menu
-  template[1].submenu.push(
-    {type: 'separator'},
-    {
-      label: 'Speech',
-      submenu: [
-        {role: 'startspeaking', label: e('startspeaking')},
-        {role: 'stopspeaking', label: e('stopspeaking')}
-      ]
-    }
-  )
+  template[1].submenu.push({
+    type: 'separator'
+  }, {
+    label: 'Speech',
+    submenu: [
+      {
+        role: 'startspeaking',
+        label: e('startspeaking')
+      },
+      {
+        role: 'stopspeaking',
+        label: e('stopspeaking')
+      }
+    ]
+  })
 
   // Window menu
   template[3].submenu = [
-    {role: 'close', label: e('close')},
-    {role: 'minimize', label: e('minimize')},
-    {role: 'zoom', label: e('zoom')},
-    {type: 'separator'},
-    {role: 'front', label: e('front')}
+    {
+      role: 'close',
+      label: e('close')
+    },
+    {
+      role: 'minimize',
+      label: e('minimize')
+    },
+    {
+      role: 'zoom',
+      label: e('zoom')
+    },
+    {
+      type: 'separator'
+    },
+    {
+      role: 'front',
+      label: e('front')
+    }
   ]
 }
 
