f87de0ad137b770977a8824c69a0893454f92d24
ZHAO Xudong <zxdong@gmail.com>
2018-07-18 17:11:03 +0800

    fix #602 use search component (#608)

--- src/client/components/common/search.jsx
+++ src/client/components/common/search.jsx
@@ -8,6 +8,7 @@ import {Input, Icon} from 'antd'
 const {Search} = Input
 
 export default class CommonSearch extends React.Component {
+
   componentDidUpdate() {
     let root = ReactDOM.findDOMNode(this)
     root.querySelector('input').focus()
