b6000b167a482ae0d8e2e3ba44fa8199087bbd10
ZHAO Xudong <zxdong@gmail.com>
2018-10-27 20:39:59 +0800

    fix #689 add file manager test, local symlink test (#834)

---
+++ test/e2e/015.terminal-symlink.spec.js
@@ -0,0 +1,65 @@
+const { Application } = require('spectron')
+const electronPath = require('electron')
+const {resolve} = require('path')
+const cwd = process.cwd()
+const delay = require('./common/wait')
+const {log} = console
+const {expect} = require('chai')
+
+describe('terminal symlink', function () {
+  this.timeout(100000)
+
+  beforeEach(async function() {
+    this.app = new Application({
+      path: electronPath,
+      webdriverOptions: {
+        deprecationWarnings: false
+      },
+      args: [resolve(cwd, 'work/app'), '--no-session-restore']
+    })
+    return this.app.start()
+  })
+
+  afterEach(function() {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+  })
+
+  it('should open window and local terminal ls/dir command works', async function() {
+    const {client} = this.app
+    let cmd = 'rm -rf tmp-o.js link-o && touch tmp-o.js && ln -s tmp-o.js link-o'
+    await client.waitUntilWindowLoaded()
+    await delay(500)
+    await client.keys([...cmd.split(''), 'Enter'])
+
+    log('click file manager tab')
+    await client.execute(function() {
+      document.querySelectorAll('.ssh-wrap-show .term-sftp-tabs .type-tab')[1].click()
+    })
+    await delay(200)
+
+    log('refresh')
+    await client.execute(function() {
+      document.querySelector('.sftp-title .anticon-reload').click()
+    })
+    await delay(800)
+
+    log('check synlink')
+    let txt = await client.getText('.sftp-table-content')
+    expect(txt.includes('*link-o')).equal(true)
+    //await delay(200)
+    //expect(has).equal(true)
+
+    //clean
+    await client.execute(function() {
+      document.querySelectorAll('.ssh-wrap-show .term-sftp-tabs .type-tab')[0].click()
+    })
+
+    await delay(200)
+    let cmd0 = 'rm -rf tmp-o.js link-o'
+    await client.keys([...cmd0.split(''), 'Enter'])
+
+  })
+
+})
