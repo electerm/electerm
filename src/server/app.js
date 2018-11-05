1db9657ef08a7979fc30584f85d9047571b91458
ZHAO Xudong <zxdong@gmail.com>
2018-11-05 16:30:06 +0800

    ff

--- src/server/app.js
+++ src/server/app.js
@@ -1,62 +1,54 @@
+
+import http from 'http'
+import {resolve} from 'path'
 import Koa from 'koa'
 import mount from 'koa-mount'
-import Bodyparser from 'koa-bodyparser'
 import logger from 'koa-logger'
 import serve from 'koa-static'
 import conditional from 'koa-conditional-get'
 import etag from 'koa-etag'
 import compress from 'koa-compress'
-import Pug from 'koa-pug-global'
 
-const isProduction = process.env.NODE_ENV === 'production'
 const cwd = process.cwd()
 const app = new Koa()
 const staticOption = () => ({
   maxAge: 1000 * 60 * 60 * 24 * 365,
   hidden: true
 })
-
-const bodyparser = Bodyparser()
-
-export default function init() {
-
-  // global middlewares
+const p = resolve(
+  process.cwd(),
+  'package.json'
+)
+const pack = require(p)
+const {
+  port = 4570,
+  host = 'localhost'
+} = process.env
+const {log} = console
+const start = function () {
   app.keys = ['electerm:' + Math.random()]
-
   app.use(compress({
     threshold: 2048,
     flush: require('zlib').Z_SYNC_FLUSH
   }))
-
-  //get
   app.use(conditional())
-
-  // add etags
   app.use(etag())
-
   app.use(
     mount(
       '/_bc',
       serve(cwd + '/node_modules', staticOption())
     )
   )
-
-  // body
-  app.use(bodyparser)
-
-  if (!isProduction) {
-    app.use(logger())
-  }
-
-  //pug template
-  new Pug({
-    viewPath: cwd + '/src/views',
-    debug: !isProduction,
-    pretty: !isProduction,
-    compileDebug: !isProduction,
-    noCache: !isProduction,
-    app: app
+  app.use(logger())
+  let server = http.Server(app.callback())
+  server.listen(port, host, () => {
+    log(`${pack.name} dev server start on --> http://${host}:${port}`)
   })
+}
 
-  return app
+try {
+  start()
+} catch (e) {
+  log(`error start ${pack.name}'`, e.stack)
+  process.exit(1)
 }
