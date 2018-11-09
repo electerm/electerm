e7ae0bc861c934616dacde95bdd4b9c2b78d2b9b
ZHAO Xudong <zxdong@gmail.com>
2018-11-09 15:20:26 +0800

    fix #863 Make context menu click auto trigger close event

---
+++ src/client/common/find-parent.js
@@ -0,0 +1,30 @@
+/**
+ * find the target parentNode
+ * @param {Node} node
+ * @param {String} className
+ * @return {Boolean}
+ */
+export default function findParentBySel(node, sel) {
+  if (!node) {
+    return false
+  }
+  let parent = node
+  if (!parent || !parent.matches) {
+    return false
+  }
+  if (parent.matches(sel)) {
+    return parent
+  }
+  let res = false
+  while (parent !== document.body) {
+    parent = parent.parentNode
+    if (!parent || !parent.matches) {
+      break
+    }
+    if (parent.matches(sel)) {
+      res = parent
+      break
+    }
+  }
+  return res
+}
