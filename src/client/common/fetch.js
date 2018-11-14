d837b97d0b52c535a795c2d87bad08a874f42f1f
ZHAO Xudong <zxdong@gmail.com>
2018-11-14 16:56:57 +0800

    fix #880 Simplify fetch wrapper (#913)

--- src/client/common/fetch.js
+++ src/client/common/fetch.js
@@ -10,14 +10,7 @@ const jsonHeader = {
 function parseResponse(response) {
   let contentType = response.headers.get('content-type') || ''
   let isJsonResult = contentType.toLowerCase().includes('application/json')
-
   return isJsonResult ? response.json() : response.text()
-}
-
-export function serialized(data) {
-  return data
-    ? `?q=${JSON.stringify(data)}`
-    : ''
 }
 
 export async function handleErr(res) {
@@ -41,32 +34,18 @@ export async function handleErr(res) {
 export default class Fetch {
 
   static get(url, data, options) {
-    return Fetch.connect(url + serialized(data), 'get', null, options)
+    return Fetch.connect(url, 'get', null, options)
   }
 
   static post(url, data, options) {
     return Fetch.connect(url, 'post', data, options)
   }
 
-  static delete(url, data, options) {
-    return Fetch.connect(url, 'delete', data, options)
-  }
-
-  static put(url, data, options) {
-    return Fetch.connect(url, 'put', data, options)
-  }
-
-  static patch(url, data, options) {
-    return Fetch.connect(url, 'patch', data, options)
-  }
-
   static connect(url, method, data, options = {}) {
     let body = {
       method,
       body: data
-        ? JSON.stringify({
-          q: JSON.stringify(data)
-        })
+        ? JSON.stringify(data)
         : undefined,
       headers: jsonHeader,
       timeout: 180000,
@@ -74,7 +53,6 @@ export default class Fetch {
     }
     return fetch(url, body)
       .then(res => {
-        window.__fetchCount --
         if (res.status > 304) {
           throw res
         }
