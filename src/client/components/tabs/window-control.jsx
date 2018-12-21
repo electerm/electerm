09a3a5435ebe898ea1f8864ab73eddfb53bb4274
ZHAO Xudong <zxdong@gmail.com>
2018-12-21 11:03:36 +0900

    fix #1026 fix window control icons behavior (#1027)

--- src/client/components/tabs/window-control.jsx
+++ src/client/components/tabs/window-control.jsx
@@ -28,30 +28,36 @@ export default memo(props => {
   }
   return (
     <div className="window-controls">
-      <Icon
-        type="minus"
-        title={m('minimize')}
-        className="mg1r iblock pointer font12 widnow-control-icon"
-        onClick={minimize}
-      />
-      <span
-        title={
-          isMaximized ? m('unmaximize') : m('maximize')
-        }
-        className={
-          'mg1r iblock pointer font12 icon-maximize widnow-control-icon ' +
-            (isMaximized ? 'is-max' : 'not-max')
-        }
-        onClick={
-          isMaximized ? unmaximize : maximize
-        }
-      />
-      <Icon
-        type="close"
-        title={m('close')}
-        className="iblock pointer font10 widnow-control-icon"
-        onClick={closeApp}
-      />
+      <div className="window-control-box">
+        <Icon
+          type="minus"
+          title={m('minimize')}
+          className="iblock font12 widnow-control-icon"
+          onClick={minimize}
+        />
+      </div>
+      <div className="window-control-box">
+        <span
+          title={
+            isMaximized ? m('unmaximize') : m('maximize')
+          }
+          className={
+            'iblock font12 icon-maximize widnow-control-icon ' +
+              (isMaximized ? 'is-max' : 'not-max')
+          }
+          onClick={
+            isMaximized ? unmaximize : maximize
+          }
+        />
+      </div>
+      <div className="window-control-box">
+        <Icon
+          type="close"
+          title={m('close')}
+          className="iblock font12 widnow-control-icon"
+          onClick={closeApp}
+        />
+      </div>
     </div>
   )
 })
