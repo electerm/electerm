f0b255438d9171ef8074dcfef0fc14aba21eafc7
ZHAO Xudong <zxdong@gmail.com>
2018-12-27 13:43:56 +0800

    fix #1020 Add tab double click test (#1040)

--- test/e2e/013.tabs.spec.js
+++ test/e2e/013.tabs.spec.js
@@ -33,6 +33,21 @@ describe('tabs', function () {
     await delay(500)
     let tabs = await client.elements('.tabs .tab')
     let tabsLenBefore = tabs.value.length
+    await client.doubleClick('.tab')
+    await delay(500)
+    let tabs0 = await client.elements('.tabs .tab')
+    expect(tabs0.value.length).equal(tabsLenBefore + 1)
+    let wraps = await client.elements('.ui-outer > div')
+    expect(wraps.value.length).equal(tabsLenBefore + 1)
+    await delay(500)
+  })
+
+  it('double click to duplicate tab button works', async function() {
+    const {client} = this.app
+    await client.waitUntilWindowLoaded()
+    await delay(500)
+    let tabs = await client.elements('.tabs .tab')
+    let tabsLenBefore = tabs.value.length
     await client.click('.tabs .tabs-add-btn')
     await delay(500)
     let tabs0 = await client.elements('.tabs .tab')
