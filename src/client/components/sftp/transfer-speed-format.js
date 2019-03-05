99d481009ea0f4da845186724f1083ee7a94fa87
ZHAO Xudong <zxdong@gmail.com>
2019-03-05 16:57:39 +0800

    [feat] Show time passed in transfer list

--- src/client/components/sftp/transfer-speed-format.js
+++ src/client/components/sftp/transfer-speed-format.js
@@ -23,24 +23,35 @@ export default (bytes, startTime) => {
   }
 }
 
-export const leftTime = (bytes, total, startTime) => {
+function formatTime(ms) {
+  let d = Math.floor(ms / day)
+  let h = Math.floor((ms - d * day) / hour)
+  let m = Math.floor((ms - d * day - h * hour) / minute)
+  let s = Math.floor((ms - d * day - h * hour - m * minute) / sec)
+  if (ms > month) {
+    return '>30d'
+  } else if (ms > day) {
+    return `${d}d ${h}:${m}:${s}`
+  } else if (ms > hour) {
+    return `${h}:${m}:${s}`
+  } else if (ms > minute) {
+    return `${m}:${s}`
+  } else {
+    return Math.floor(ms / 1000) + 's'
+  }
+}
+
+export const computePassedTime = (startTime) => {
+  let allTimeNeed = (+new Date()) - startTime
+  return formatTime(allTimeNeed)
+}
+
+export const computeLeftTime = (bytes, total, startTime) => {
   let now = +new Date()
   if (now <= startTime) {
     now = startTime + 1
   }
   let speed = bytes / (now - startTime)
   let allTimeNeed = (total - bytes) / speed
-  let d = Math.floor(allTimeNeed / day)
-  let h = Math.floor((allTimeNeed - d * day) / hour)
-  let m = Math.floor((allTimeNeed - d * day - h * hour) / minute)
-  let s = Math.floor((allTimeNeed - d * day - h * hour - m * minute) / sec)
-  if (allTimeNeed > month) {
-    return '>30d'
-  } else if (allTimeNeed > day) {
-    return `${d}d ${h}:${m}:${s}`
-  } else if (allTimeNeed > hour) {
-    return `${h}:${m}:${s}`
-  } else if (allTimeNeed > minute) {
-    return `${m}:${s}`
-  }
+  return formatTime(allTimeNeed)
 } 
