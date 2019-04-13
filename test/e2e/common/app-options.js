5e8291e3c5889cb569d556483b87081400a9d163
ZHAO Xudong <zxdong@gmail.com>
2019-04-14 02:48:06 +0800

    [skip appveyor]1

--- test/e2e/common/app-options.js
+++ test/e2e/common/app-options.js
@@ -2,12 +2,13 @@
 const electronPath = require('electron')
 const {resolve} = require('path')
 const cwd = process.cwd()
+const isCI = !!process.env.GH_TOKEN
 
 module.exports = {
   path: electronPath,
   webdriverOptions: {
     deprecationWarnings: false
   },
-  chromeDriverArgs: ['remote-debugging-port=9222'],
+  chromeDriverArgs: isCI ? [] : ['remote-debugging-port=9222'],
   args: [resolve(cwd, 'work/app'), '--no-session-restore']
 }
