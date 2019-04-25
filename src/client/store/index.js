de5267968d3c124ace435814fc1c4f7f69d2a305
ZHAO Xudong <zxdong@gmail.com>
2019-04-25 16:05:23 +0800

    save

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -3,37 +3,25 @@
  */
 
 import Subx from 'subx'
-//import React from 'react'
 import {message} from 'antd'
-// import Session from '../session'
-// import Tabs from '../tabs'
 import newTerm from '../../common/new-terminal'
 import _ from 'lodash'
-//import {generate} from 'shortid'
+import {generate} from 'shortid'
 import copy from 'json-deep-copy'
-// import ContextMenu from '../common/context-menu'
-// import FileInfoModal from '../sftp/file-props-modal'
-// import wait from '../../common/wait'
-// import FileModeModal from '../sftp/file-mode-modal'
-// import UpdateCheck from './upgrade'
-// import {notification} from 'antd'
-// import SettingModal from '../setting-panel/setting-modal'
-// import openInfoModal from '../sidebar/info-modal'
+import wait from '../../common/wait'
+import {notification} from 'antd'
+import openInfoModal from '../sidebar/info-modal'
 import * as terminalThemes from '../../common/terminal-theme'
-// import createTitlte from '../../common/create-title'
-// import TextEditor from '../text-editor'
-// import Sidebar from '../sidebar'
-// import SystemMenu from './system-menu'
+import createTitlte from '../../common/create-title'
 import {
-  //maxHistory,
+  maxHistory,
   settingMap,
   defaultookmarkGroupId,
-  //maxTransferHistory,
+  maxTransferHistory,
   sidebarWidth,
   statusMap,
   defaultTheme
 } from '../../common/constants'
-//import SessionControl from '../session-control'
 import {buildNewTheme} from '../../common/terminal-theme'
 import './wrapper.styl'
 
@@ -42,9 +30,9 @@ const ls = getGlobal('ls')
 const {prefix} = window
 const t = prefix('terminalThemes')
 const e = prefix('control')
-// const defaultStatus = statusMap.processing
-// let sessionsGlob = copy(ls.get('sessions'))
-// const sshConfigItems = copy(getGlobal('sshConfigItems'))
+const defaultStatus = statusMap.processing
+let sessionsGlob = copy(ls.get('sessions'))
+const sshConfigItems = copy(getGlobal('sshConfigItems'))
 const getInitItem = (arr, tab) => {
   if (tab === settingMap.history) {
     return arr[0] || {}
@@ -150,7 +138,7 @@ const store = Subx.create({
     store.modifier({
       tabs
     })
-  }//,
+  },
 
   // onResize = _.debounce(() => {
   //   let update = {
@@ -177,7 +165,7 @@ const store = Subx.create({
   //   store.setStateLs(...args)
   // }
   //todo subscribe
-/*
+
   modifier (...args) {
     let update = args[0]
     let changed = (update.currentTabId && update.currentTabId !== store.currentTabId) || update.tabs
@@ -192,22 +180,22 @@ const store = Subx.create({
     if (update.tabs) {
       ls.set('sessions', update.tabs)
     }
-  }
+  },
   //todo use subscribe
 
-  initContextEvent = () => {
+  initContextEvent () {
     let dom = document.getElementById('outside-context')
     store.dom = dom
     dom.addEventListener('click', store.closeContextMenu)
-  }
+  },
 
-  initMenuEvent = () => {
+  initMenuEvent () {
     let dom = document.getElementById('outside-context')
     store.dom = dom
     dom.addEventListener('click', store.closeMenu)
-  }
+  },
 
-  selectall = () => {
+  selectall () {
     document.activeElement &&
     document.activeElement.select &&
     document.activeElement.select()
@@ -215,9 +203,9 @@ const store = Subx.create({
       event: 'selectall',
       id: store.activeTerminalId
     }, '*')
-  }
+  },
 
-  zoom = (level = 1, plus = false, zoomOnly) => {
+  zoom (level = 1, plus = false, zoomOnly) {
     let {webFrame} = require('electron')
     let nl = plus
       ? webFrame.getZoomFactor() + level
@@ -236,23 +224,9 @@ const store = Subx.create({
         config: nc
       }
     })
-  }
+  },
 
-  clickNextTab = _.debounce(() => {
-    let tab = document.querySelector('.tabs-wrapper .tab.active')
-    if (tab) {
-      let next = tab.nextSibling
-      if (!next || !next.classList.contains('tab')) {
-        next = document.querySelector('.tabs-wrapper .tab')
-      }
-      next &&
-      next.querySelector('.tab-title') &&
-      next.querySelector('.tab-title').click()
-    }
-  }, 100)
-  //todo
-
-  getDefaultBookmarkGroups = (bookmarks) => {
+  getDefaultBookmarkGroups (bookmarks) {
     return [
       {
         title: t(defaultookmarkGroupId),
@@ -260,17 +234,17 @@ const store = Subx.create({
         bookmarkIds: bookmarks.map(d => d.id)
       }
     ]
-  }
+  },
 
-  checkLastSession = () => {
+  checkLastSession () {
     let status = window.getGlobal('getExitStatus')()
     if (status === 'ok') {
       return
     }
     store.showLastSessions(sessionsGlob)
-  }
+  },
 
-  showLastSessions = sessions => {
+  showLastSessions (sessions) {
     if (!sessions) {
       return
     }
@@ -282,37 +256,37 @@ const store = Subx.create({
       })),
       sessionModalVisible: true
     })
-  }
+  },
 
-  openAbout = () => {
+  openAbout () {
     openInfoModal({
       onCheckUpdating: store.upgradeInfo.checkingRemoteVersion || store.upgradeInfo.upgrading,
       onCheckUpdate: store.onCheckUpdate,
       onCancel: store.focus,
       onOk: store.focus
     })
-  }
+  },
 
-  openTransferHistory = () => {
+  openTransferHistory () {
     store.setState({
       transferHistoryModalVisible: true
     })
-  }
+  },
 
-  closeTransferHistory = () => {
+  closeTransferHistory () {
     store.setState({
       transferHistoryModalVisible: false
     })
-  }
+  },
 
-  clearTransferHistory = () => {
+  clearTransferHistory () {
     store.setState({
       transferHistory: [],
       transferHistoryModalVisible: false
     })
-  }
+  },
 
-  addTransferHistory = (item) => {
+  addTransferHistory (item) {
     let transferHistory = [
       item,
       ...copy(store.transferHistory)
@@ -320,33 +294,33 @@ const store = Subx.create({
     store.setState({
       transferHistory
     })
-  }
+  },
 
-  onCheckUpdate = () => {
+  onCheckUpdate () {
     if (store.onCheckUpdating) {
       return
     }
     store.setState({
       shouldCheckUpdate: +new Date()
     })
-  }
+  },
 
-  openContextMenu = (contextMenuProps) => {
+  openContextMenu (contextMenuProps) {
     store.setState({
       contextMenuProps,
       contextMenuVisible: true
     })
     store.initContextEvent()
-  }
+  },
 
-  closeContextMenu = () => {
+  closeContextMenu () {
     store.setState({
       contextMenuVisible: false
     })
     store.dom && store.dom.removeEventListener('click', store.closeContextMenu)
-  }
+  },
 
-  onError = e => {
+  onError (e) {
     let {message = 'error', stack} = e
     log.error(e)
     let msg = (
@@ -367,17 +341,17 @@ const store = Subx.create({
       placement: 'bottomRight',
       duration: 55
     })
-  }
+  },
 
-  addItem = (item, type) => {
+  addItem (item, type) {
     let items = store[type]
     items.unshift(item)
     if (type === settingMap.history && items.length > maxHistory) {
       items = items.slice(0, maxHistory)
     }
-  }
+  },
 
-  editItem = (id, update, type) => {
+  editItem (id, update, type) {
     let items = store[type]
     let item = _.find(items, t => t.id === id)
     if (!item) {
@@ -386,23 +360,23 @@ const store = Subx.create({
     let index = _.findIndex(items, t => t.id === id)
     Object.assign(item, update)
     items.splice(index, 1, item)
-  }
+  },
 
-  delItem = ({id}, type) => {
+  delItem ({id}, type) {
     store[type] = store[type].filter(t => {
       return t.id !== id
     })
-  }
+  },
 
-  addTab = (tab = newTerm(), index = store.tabs.length) => {
+  addTab (tab = newTerm(), index = store.tabs.length) {
     store.tabs.splice(index, 0, tab)
-  }
+  },
 
-  editTab = (id, update) => {
+  editTab (id, update) {
     store.editItem(id, update, 'tabs', store.modifier)
-  }
+  },
 
-  delTab = ({id}) => {
+  delTab ({id}) {
     store.tabs = store.tabs.filter(t => {
       return t.id !== id
     })
@@ -411,28 +385,21 @@ const store = Subx.create({
       let next = tabs[0] || {}
       store.currentTabId = next.id
     }
-  }
+  },
 
-  addTheme = (theme) => {
+  addTheme (theme) {
     store.themes.unshift(theme)
-    themes = [
-      theme,
-      ...themes
-    ]
-    store.setState({
-      themes
-    })
     terminalThemes.addTheme(theme)
-  }
+  },
 
-  editTheme = (id, update) => {
+  editTheme (id, update) {
     let items = store.themes
     let item = _.find(items, t => t.id === id)
     Object.assign(item, update)
     terminalThemes.updateTheme(id, update)
-  }
+  },
 
-  delTheme = ({id}) => {
+  delTheme ({id}) {
     store.themes = store.themes.filter(t => {
       return t.id !== id
     })
@@ -441,19 +408,19 @@ const store = Subx.create({
       store.theme = terminalThemes.defaultTheme.id
     }
     terminalThemes.delTheme(id)
-  }
+  },
 
-  addBookmarkGroup = (group) => {
+  addBookmarkGroup (group) {
     store.bookmarkGroups.unshift(group)
-  }
+  },
 
-  editBookmarkGroup = (id, update) => {
+  editBookmarkGroup (id, update) {
     let items = store.bookmarkGroups
     let item = _.find(items, t => t.id === id)
     Object.assign(item, update)
-  }
+  },
 
-  delBookmarkGroup = ({id}) => {
+  delBookmarkGroup ({id}) {
     if (id === defaultookmarkGroupId) {
       return
     }
@@ -501,23 +468,23 @@ const store = Subx.create({
     if (id === store.currentBookmarkGroupId) {
       store.currentBookmarkGroupId = ''
     }
-  }
+  },
 
-  setTheme = id => {
+  setTheme (id) {
     store.theme = id
     terminalThemes.setTheme(id)
-  }
+  },
 
-  onDelItem = (item, type) => {
+  onDelItem (item, type) {
     if (item.id === store.item.id) {
       store.item = getInitItem(
-        old[type],
+        store[type],
         type
       )
     }
-  }
+  },
 
-  reloadTab = async (tabToReload) => {
+  async reloadTab (tabToReload) {
     let tab = copy(
       tabToReload
     )
@@ -529,9 +496,9 @@ const store = Subx.create({
     store.delTab({id: tabToReload.id})
     await wait(30)
     store.addTab(tab, index)
-  }
+  },
 
-  onDuplicateTab = tab => {
+  onDuplicateTab (tab) {
     let index = _.findIndex(
       store.tabs,
       d => d.id === tab.id
@@ -541,31 +508,31 @@ const store = Subx.create({
       status: defaultStatus,
       id: generate()
     }, index + 1)
-  }
+  },
 
-  onChangeTabId = currentTabId => {
+  onChangeTabId (currentTabId) {
     store.currentTabId = currentTabId
-  }
+  },
 
-  onNewSsh = () => {
+  onNewSsh () {
     store.setState({
       tab: settingMap.bookmarks,
       item: getInitItem([], settingMap.bookmarks),
       autofocustrigger: + new Date()
     })
     store.openModal()
-  }
+  },
 
-  onEditHistory = () => {
+  onEditHistory () {
     store.setState({
       tab: settingMap.history,
       item: store.history[0] || getInitItem([], settingMap.history),
       autofocustrigger: + new Date()
     })
     store.openModal()
-  }
+  },
 
-  onSelectHistory = id => {
+  onSelectHistory (id) {
     let item = _.find(store.history, it => it.id === id)
     store.addTab({
       ...copy(item),
@@ -574,9 +541,9 @@ const store = Subx.create({
       status: defaultStatus,
       id: generate()
     })
-  }
+  },
 
-  onSelectBookmark = id => {
+  onSelectBookmark (id) {
     let {history, bookmarks} = store
     let item = copy(
       _.find(bookmarks, it => it.id === id) ||
@@ -607,49 +574,45 @@ const store = Subx.create({
     if (!existItem) {
       store.addItem(item, settingMap.history)
     } else {
-      let historyNew = copy(history)
-      let index = _.findIndex(historyNew, f => f.id === existItem.id)
-      historyNew.splice(index, 1)
-      historyNew.unshift(existItem)
-      store.modifier({history: historyNew})
+      let index = _.findIndex(history, f => f.id === existItem.id)
+      history.splice(index, 1)
+      history.unshift(existItem)
     }
-  }
+  },
 
-  openSetting = () => {
+  openSetting () {
     store.setState({
       tab: settingMap.setting,
       item: getInitItem([], settingMap.setting)
-    }, store.openModal)
-  }
+    })
+    store.openModal()
+  },
 
-  openTerminalThemes = () => {
+  openTerminalThemes () {
     store.setState({
       tab: settingMap.terminalThemes,
       item: buildNewTheme(),
       autofocustrigger: + new Date()
-    }, store.openModal)
-  }
-
-  openModal = () => {
-    store.setState({
-      showModal: true
     })
-  }
+    store.openModal()
+  },
 
-  hideModal = () => {
-    store.setState({
-      showModal: false
-    })
+  openModal () {
+    store.showModal = true
+  },
+
+  hideModal () {
+    store.showModal = false
     store.focus()
-  }
+  },
 
-  getItems = (tab, props = store) => {
+  getItems (tab, props = store) {
     return tab === settingMap.terminalThemes
       ? copy(props.themes)
       : copy(props[tab]) || []
-  }
+  },
 
-  onChangeTab = tab => {
+  onChangeTab (tab) {
     let arr = store.getItems(tab)
     let item = getInitItem(arr, tab)
     store.setState({
@@ -657,9 +620,9 @@ const store = Subx.create({
       autofocustrigger: + new Date(),
       tab
     })
-  }
+  },
 
-  getList = () => {
+  getList () {
     let {
       tab
     } = store
@@ -672,5 +635,17 @@ const store = Subx.create({
         ...arr
       ]
   }
-  */
 })
+
+store.clickNextTab = _.debounce(() => {
+  let tab = document.querySelector('.tabs-wrapper .tab.active')
+  if (tab) {
+    let next = tab.nextSibling
+    if (!next || !next.classList.contains('tab')) {
+      next = document.querySelector('.tabs-wrapper .tab')
+    }
+    next &&
+    next.querySelector('.tab-title') &&
+    next.querySelector('.tab-title').click()
+  }
+}, 100)
