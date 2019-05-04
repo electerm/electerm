1793923aa3848fd61ff7c5493bca594b98452bd7
ZHAO Xudong <zxdong@gmail.com>
2019-05-05 03:11:10 +0800

    [skip travis]

--- bin/post-install.js
+++ bin/post-install.js
@@ -1,12 +1,11 @@
 /**
  * post install script
  */
-const {exec, cp, rm} = require('shelljs')
+const {cp, rm} = require('shelljs')
 const {existsSync} = require('fs')
 const {resolve} = require('path')
 const prePushPath = resolve(__dirname, '../.git/hooks/pre-push')
 const prePushPathFrom = resolve(__dirname, 'pre-push')
-exec(resolve('./node_modules/.bin/electron-rebuild'))
 
 if (!existsSync(prePushPath)) {
   cp(prePushPathFrom, prePushPath)
