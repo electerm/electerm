9735a69bbaeeab29ac79d8c9e5db1761e472dbbc
ZHAO Xudong <zxdong@gmail.com>
2019-06-10 10:46:50 +0800

    [skip build][skip appveyor]

--- test/e2e/common/app-options.js
+++ test/e2e/common/app-options.js
@@ -9,6 +9,6 @@ module.exports = {
     deprecationWarnings: false
   },
   chromeDriverArgs: ['remote-debugging-port=9222'],
-  chromeDriverLogPath: '/Users/drake.zhao/dev/electerm/temp/w.log',
+  chromeDriverLogPath: resolve(cwd, 'x.log'),
   args: [resolve(cwd, 'work/app'), '--no-session-restore']
 }
