3fcc0052fb092de213caeb5c3cc9984343fd2ee7
ZHAO Xudong <zxdong@gmail.com>
2019-07-15 14:58:03 +0800

    Update spectron (#1313)

--- bin/post-install.js
+++ bin/post-install.js
@@ -1,7 +1,7 @@
 /**
  * post install script
  */
-const { cp, rm, exec } = require('shelljs')
+const { cp, exec } = require('shelljs')
 const { existsSync } = require('fs')
 const { resolve } = require('path')
 const prePushPath = resolve(__dirname, '../.git/hooks/pre-push')
@@ -12,6 +12,3 @@ exec(resolve('./node_modules/.bin/electron-rebuild'))
 if (!existsSync(prePushPath)) {
   cp(prePushPathFrom, prePushPath)
 }
-
-// fix spectron's electron-chromedriver deps
-rm('-rf', './node_modules/spectron/node_modules')
