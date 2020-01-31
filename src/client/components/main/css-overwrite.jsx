45ae767efc5843681d0e28bcaf78357065aae3aa
ZHAO Xudong <zxdong@gmail.com>
2020-01-31 19:51:53 +0800

    fix #1539 [feat] Add no-terminal-background option for terminal background

--- src/client/components/main/css-overwrite.jsx
+++ src/client/components/main/css-overwrite.jsx
@@ -4,6 +4,7 @@
 
 import { PureComponent } from 'react'
 import fs from '../../common/fs'
+import { noTerminalBgValue } from '../../common/constants'
 
 export default class SystemMenu extends PureComponent {
   componentDidMount () {
@@ -16,14 +17,14 @@ export default class SystemMenu extends PureComponent {
     }
   }
 
-  writeCss = async () => {
-    const style = document.createElement('style')
-    style.type = 'text/css'
+  createStyle = async () => {
     const { terminalBackgroundImagePath } = this.props
     let content = ''
     let st = ''
     const isWebImg = /^https?:\/\//.test(terminalBackgroundImagePath)
-    if (terminalBackgroundImagePath && !isWebImg) {
+    if (noTerminalBgValue === terminalBackgroundImagePath) {
+      st = 'none'
+    } else if (terminalBackgroundImagePath && !isWebImg) {
       content = await fs.readFileAsBase64(terminalBackgroundImagePath)
         .catch(console.log)
       if (content) {
@@ -32,32 +33,26 @@ export default class SystemMenu extends PureComponent {
     } else if (terminalBackgroundImagePath && isWebImg) {
       st = `url(${terminalBackgroundImagePath})`
     }
-    style.innerHTML = st
+    return st
       ? `
     #container .xterm-viewport {
       background-image: ${st};
     }
     `
       : ''
+  }
+
+  writeCss = async () => {
+    const style = document.createElement('style')
+    style.type = 'text/css'
+    style.innerHTML = await this.createStyle()
     style.id = 'css-overwrite'
     document.getElementsByTagName('head')[0].appendChild(style)
   }
 
   updateCss = async () => {
     const style = document.getElementById('css-overwrite')
-    const { terminalBackgroundImagePath } = this.props
-    let content = ''
-    if (terminalBackgroundImagePath) {
-      content = await fs.readFileAsBase64(terminalBackgroundImagePath)
-        .catch(console.log)
-    }
-    style.innerHTML = content
-      ? `
-    #container .xterm .xterm-viewport {
-      background-image: url(data:image;base64,${content});
-    }
-    `
-      : ''
+    style.innerHTML = await this.createStyle()
   }
 
   render () {
