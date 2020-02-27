6caadff59af0108996b7800f482b3b5768dd3572
ZHAO Xudong <zxdong@gmail.com>
2020-02-27 22:58:28 +0800

    fix #1576 [UI] Highlight active split terminal (#1577)

--- src/client/components/terminal/index.jsx
+++ src/client/components/terminal/index.jsx
@@ -985,10 +985,12 @@ export default class Term extends Component {
 
   render () {
     const { id, loading, zmodemTransfer } = this.state
-    const { height, width, left, top, position, id: pid } = this.props
+    const { height, width, left, top, position, id: pid, activeSplitId } = this.props
     const cls = classnames('term-wrap', {
       'not-first-term': !!position
-    }, 'tw-' + pid)
+    }, 'tw-' + pid, {
+      'terminal-not-active': activeSplitId !== pid
+    })
     return (
       <div
         className={cls}
