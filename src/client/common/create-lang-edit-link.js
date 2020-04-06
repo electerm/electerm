6b0164a7fc9cdc7c767ecc54986d670845a54e12
ZHAO Xudong <zxdong@gmail.com>
2020-04-06 20:48:56 +0800

    fix #1655 [feat] Add edit language file link in settings

---
+++ src/client/common/create-lang-edit-link.js
@@ -0,0 +1,8 @@
+/**
+ * create edit language file link
+ */
+
+export default (lang) => {
+  console.log(lang)
+  return `https://github.com/electerm/electerm-locales/edit/master/locales/${lang}.js`
+}
