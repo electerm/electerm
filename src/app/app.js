b938803f205c0fbd0e17c8d79d5552f7798f7233
ZHAO Xudong <zxdong@gmail.com>
2020-05-13 20:16:13 +0800

    fix #1692 [fix] Quit properly kill child process (#1693)

--- src/app/app.js
+++ src/app/app.js
@@ -231,9 +231,6 @@ async function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
-app.on('window-all-closed', onClose)
-
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
