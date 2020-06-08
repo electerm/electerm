a978bec163e4d75b1fda8f3531ed75b4db566941
ZHAO Xudong <zxdong@gmail.com>
2020-06-08 14:21:11 +0800

    fix #1716 [fix] Catch tray icon click error (#1732)

--- src/app/lib/tray.js
+++ src/app/lib/tray.js
@@ -5,6 +5,7 @@
 const { app, Tray } = require('electron')
 const { trayIconPath, packInfo, minWindowHeight, minWindowWidth } = require('../utils/app-props')
 const menu = require('./menu')
+const log = require('../utils/log')
 
 let tray = null
 app.on('ready', () => {
@@ -12,12 +13,17 @@ app.on('ready', () => {
   tray.setToolTip(packInfo.name)
   tray.setIgnoreDoubleClickEvents(true)
   function onClick () {
-    const [w, h] = global.win.getSize()
-    global.win.show()
-    if (w < minWindowWidth || h < minWindowHeight) {
-      global.win.maximize()
+    try {
+      const [w, h] = global.win.getSize()
+      global.win.show()
+      if (w < minWindowWidth || h < minWindowHeight) {
+        global.win.maximize()
+      }
+      tray.popUpContextMenu(menu)
+    } catch (e) {
+      log.error('click tray icon error')
+      log.error(e)
     }
-    tray.popUpContextMenu(menu)
   }
   tray.on('click', onClick)
   tray.on('right-click', onClick)
