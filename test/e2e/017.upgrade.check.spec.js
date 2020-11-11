b2402584b0192491faecf42074f3539e91ece146
ZHAO Xudong <zxdong@gmail.com>
2020-11-11 12:53:54 +0800

    test: Add upgrade check test (#1879)

---
+++ test/e2e/017.upgrade.check.spec.js
@@ -0,0 +1,42 @@
+const { Application } = require('spectron')
+const delay = require('./common/wait')
+const log = require('./common/log')
+const { expect } = require('chai')
+const appOptions = require('./common/app-options')
+const extendClient = require('./common/client-extend')
+
+describe('init setting buttons', function () {
+  this.timeout(100000)
+
+  beforeEach(async function () {
+    this.app = new Application(appOptions)
+    return this.app.start()
+  })
+
+  afterEach(async function () {
+    if (this.app && this.app.isRunning()) {
+      await this.app.stop()
+      return true
+    }
+  })
+
+  it('Upgrade check should work', async function () {
+    const { client } = this.app
+    client.element = client.$
+    extendClient(client)
+    await client.waitUntilWindowLoaded()
+    await delay(500)
+
+    log('button:edit')
+    await client.click('.btns .open-about-icon')
+    await delay(500)
+    const sel = '.ant-modal .ant-tabs-nav-list .ant-tabs-tab-active'
+    const active = await client.element(sel)
+    expect(!!active.elementId).equal(true)
+
+    await client.click('.about-wrap .ant-btn-primary')
+    await delay(1000)
+    const up = await client.element('.upgrade-panel')
+    expect(!!up.elementId).equal(true)
+  })
+})
