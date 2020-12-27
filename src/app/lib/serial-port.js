3ad7c28b61760a50d5d9023f02ba1d2dbd6c9c69
ZHAO Xudong <zxdong@gmail.com>
2020-12-27 19:28:55 +0800

    Fix dependencies (#1991)

--- src/app/lib/serial-port.js
+++ src/app/lib/serial-port.js
@@ -1,12 +1,12 @@
 /**
  * serial port lib
  */
-const SerialPort = require('serialport')
+// const SerialPort = require('serialport')
 // const MockBinding = require('@serialport/binding-mock')
 
 // SerialPort.Binding = MockBinding
 // MockBinding.createPort('/dev/ROBOT', { echo: true, record: true })
 
 exports.listSerialPorts = function () {
-  return SerialPort.list()
+  return require('serialport').list()
 }
