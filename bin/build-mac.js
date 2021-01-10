adc92e63d1babaed9116c407aab5c849bc31278e
ZHAO Xudong <zxdong@gmail.com>
2021-01-10 17:04:43 +0800

    [skip linux][skip appveyor]6

--- bin/build-mac.js
+++ bin/build-mac.js
@@ -1,4 +1,9 @@
 const { exec } = require('shelljs')
 
-const cmd = './node_modules/.bin/electron-builder --mac'
+const cmd = 'rm -rf dist && ' +
+'echo "build x64 mac" && ' +
+'./node_modules/.bin/electron-builder --mac --x64 && ' +
+'echo "build arm64 mac" && ' +
+'./node_modules/.bin/electron-rebuild --arch arm64 -f -p work/app && ' +
+'./node_modules/.bin/electron-builder --mac --arm64'
 exec(cmd)
