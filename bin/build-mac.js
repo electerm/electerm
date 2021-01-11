71de4e1780c699237ba83c4dcd27b532bd07555b
ZHAO Xudong <zxdong@gmail.com>
2021-01-11 09:05:21 +0800

    Update build config

--- bin/build-mac.js
+++ bin/build-mac.js
@@ -1,9 +1,10 @@
 const { exec } = require('shelljs')
 
-const cmd = 'rm -rf dist && ' +
-'echo "build x64 mac" && ' +
-'./node_modules/.bin/electron-builder --mac --x64 && ' +
-'echo "build arm64 mac" && ' +
-'./node_modules/.bin/electron-rebuild --arch arm64 -f -p work/app && ' +
-'./node_modules/.bin/electron-builder --mac --arm64'
+// const cmd = 'rm -rf dist && ' +
+// 'echo "build x64 mac" && ' +
+// './node_modules/.bin/electron-builder --mac --x64 && ' +
+// 'echo "build arm64 mac" && ' +
+// './node_modules/.bin/electron-rebuild --arch arm64 -f -p work/app && ' +
+// './node_modules/.bin/electron-builder --mac --arm64'
+const cmd = './node_modules/.bin/electron-builder --mac'
 exec(cmd)
