929a50f50b2609353793ab42a81d286a63e4d7ef
ZHAO Xudong <zxdong@gmail.com>
2021-01-27 15:01:16 +0800

    Remove arm-mac build

--- bin/build-mac.js
+++ bin/build-mac.js
@@ -2,9 +2,9 @@ const { exec } = require('shelljs')
 
 const cmd = 'rm -rf dist && ' +
 'echo "build x64 mac" && ' +
-'./node_modules/.bin/electron-builder --mac --x64 && ' +
-'echo "build arm64 mac" && ' +
+'./node_modules/.bin/electron-builder --mac --x64'
+// 'echo "build arm64 mac" && ' +
 // './node_modules/.bin/electron-rebuild --arch arm64 -f -p work/app && ' +
-'./node_modules/.bin/electron-builder --mac --arm64'
+// './node_modules/.bin/electron-builder --mac --arm64'
 // const cmd = './node_modules/.bin/electron-builder --mac'
 exec(cmd)
