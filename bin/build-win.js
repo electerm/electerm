de75875f93e8db5f3e7c0ab88c6a092e1e52807a
ZHAO Xudong <zxdong@gmail.com>
2021-08-08 21:34:37 +0800

    Refractor upgrade module (#2209)

--- bin/build-win.js
+++ bin/build-win.js
@@ -1,19 +1,53 @@
-const { exec } = require('shelljs')
+
 const replace = require('replace-in-file')
+const { rm, echo } = require('shelljs')
+const {
+  run,
+  writeSrc,
+  builder
+} = require('./build-common')
 const options = {
   files: require('path').resolve(__dirname, '../electron-builder.json'),
   from: ['"asar": true', '${productName}-${version}-${os}-${arch}.${ext}', ', "appx", "nsis"'], // eslint-disable-line
   to: ['"asar": false', '${productName}-${version}-${os}-${arch}-loose.${ext}', ''] // eslint-disable-line
 }
 
-const cmd = 'npm run output -w'
-// 'echo "build arm64 mac" && ' +
-// './node_modules/.bin/electron-rebuild --arch arm64 -f -p work/app && ' +
-// './node_modules/.bin/electron-builder --mac --arm64'
-// const cmd = './node_modules/.bin/electron-builder --mac'
-exec(cmd)
+function replaceRun () {
+  return new Promise((resolve, reject) => {
+    replace(options, (err) => {
+      if (err) {
+        return reject(err)
+      }
+      console.log('start build loose file(no asar)')
+      resolve()
+    })
+  })
+}
 
-replace(options, () => {
-  console.log('start build loose file(no asar)')
-  exec(cmd)
-})
+async function main () {
+  const pb = builder
+  echo('running build for win')
+
+  echo('build tar.gz')
+  rm('-rf', 'dist')
+  writeSrc('win-x64.tar.gz')
+  await run(`${pb} --win tar.gz`)
+
+  echo('build appx')
+  rm('-rf', 'dist')
+  writeSrc('.appx')
+  await run(`${pb} --win appx`)
+
+  echo('build nsis')
+  rm('-rf', 'dist')
+  writeSrc('win-x64-installer.exe')
+  await run(`${pb} --win nsis`)
+
+  echo('build loose tar.gz')
+  await replaceRun()
+  rm('-rf', 'dist')
+  writeSrc('win-x64-loose.tar.gz')
+  await run(`${pb} --win tar.gz`)
+}
+
+main()
