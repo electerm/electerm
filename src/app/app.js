2341d0ad79b73663bda4b30fec6b69bda17bea10
ZHAO Xudong <zxdong@gmail.com>
2021-11-08 22:15:49 +0800

    [skip win][skip linux]14

--- src/app/app.js
+++ src/app/app.js
@@ -104,30 +104,29 @@ async function createWindow () {
 }
 
 const useStandAloneWindow = initCommandLine()?.options?.nw
-let gotTheLock = true
+let gotTheLock = false
 if (!useStandAloneWindow) {
   gotTheLock = app.requestSingleInstanceLock()
-  if (gotTheLock) {
-    app.on('second-instance', (event, argv, wd) => {
-      const prog = parseCommandLine(argv)
-      const opts = {
-        options: prog.opts(),
-        argv,
-        helpInfo: prog.helpInformation()
-      }
-      if (global.win) {
-        if (global.win.isMinimized()) {
-          global.win.restore()
-        }
-        global.win.focus()
-        global.win.webContents.send('add-tab-from-command-line', opts)
-      }
-    })
-  }
 }
 if (!gotTheLock && !useStandAloneWindow) {
   app.quit()
 }
+
+app.on('second-instance', (event, argv, wd) => {
+  const prog = parseCommandLine(argv)
+  const opts = {
+    options: prog.opts(),
+    argv,
+    helpInfo: prog.helpInformation()
+  }
+  if (global.win) {
+    if (global.win.isMinimized()) {
+      global.win.restore()
+    }
+    global.win.focus()
+    global.win.webContents.send('add-tab-from-command-line', opts)
+  }
+})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
