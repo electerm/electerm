2413f33067009e33b0674da12e2314015dd1d53e
ZHAO Xudong <zxdong@gmail.com>
2022-05-04 22:36:31 +0800

    [skip linux][skip win][skip arm] Fix test

--- test/e2e/017.upgrade.check.spec.js
+++ test/e2e/017.upgrade.check.spec.js
@@ -23,7 +23,7 @@ describe('Upgrade check', function () {
     await client.hasElem(sel)
 
     await client.click('.about-wrap .ant-btn-primary')
-    await delay(5000)
+    await delay(9000)
     await client.hasElem('.upgrade-panel')
     await electronApp.close().catch(console.log)
   })
