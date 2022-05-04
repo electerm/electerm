3397afc8a82cd777e7fe0a7f5fd2be1dfd6af4f7
ZHAO Xudong <zxdong@gmail.com>
2022-05-04 21:48:16 +0800

    fix #2478 [fix] Fix rz/sz (#2481)

--- src/client/entry/index.jsx
+++ src/client/entry/index.jsx
@@ -1,12 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import { render } from 'react-dom'
 import '../../../node_modules/xterm/css/xterm.css'
 import Main from '../components/main'
 import { notification } from 'antd'
-
 notification.config({
   placement: 'bottomRight'
 })
 
-const container = document.getElementById('container')
-const root = createRoot(container)
-root.render(<Main />)
+const rootElement = document.getElementById('container')
+render(
+  <Main />,
+  rootElement
+)
