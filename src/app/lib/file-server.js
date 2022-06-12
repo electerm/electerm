843d066067345cb631a63cef8ab816e04f310d56
ZHAO Xudong <zxdong@gmail.com>
2022-06-12 22:53:56 +0800

    Improve load time

--- src/app/lib/file-server.js
+++ src/app/lib/file-server.js
@@ -3,15 +3,20 @@ const express = require('express')
 module.exports = () => {
   return new Promise((resolve) => {
     const app = express()
+    const conf = {
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    }
     app.use(
       express.static(
-        require('path').resolve(__dirname, '../assets')
+        require('path').resolve(__dirname, '../assets'),
+        conf
       )
     )
     app.use(
       '/node_modules',
       express.static(
-        require('path').resolve(__dirname, '../node_modules')
+        require('path').resolve(__dirname, '../node_modules'),
+        conf
       )
     )
     const { devPort = 5571 } = process.env
