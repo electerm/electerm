f92435cee45b5f7167480f1ef2753e062398433d
ZHAO Xudong <zxdong@gmail.com>
2022-07-24 20:26:49 +0800

    Improve local module (#2575)

--- src/app/lib/locales.js
+++ src/app/lib/locales.js
@@ -4,7 +4,6 @@
 
 const { isDev, defaultLang } = require('../common/runtime-constants')
 const fs = require('fs')
-const _ = require('lodash')
 const { resolve } = require('path')
 
 function getOsLocale () {
@@ -27,9 +26,9 @@ async function loadLocales () {
       const lang = require(filePath)
       return {
         path: filePath,
-        id: fileName.replace('.json', ''),
+        id: fileName.replace('.js', ''),
         name: lang.name,
-        match: lang.match,
+        reg: lang.match,
         lang: lang.lang
       }
     })
@@ -47,13 +46,7 @@ async function loadLocales () {
 function findLang (langs, la) {
   let res = false
   for (const l of langs) {
-    if (_.isRegExp(l.match)) {
-      res = l.match.test(la)
-    } else if (_.isFunction(l.match)) {
-      res = l.match(la)
-    } else {
-      res = l.id === la
-    }
+    res = new RegExp(l.reg).test(la)
     if (res) {
       res = l.id
       break
@@ -62,13 +55,13 @@ function findLang (langs, la) {
   return res
 }
 
-const getLang = (config, sysLocale) => {
+const getLang = (config, sysLocale, langs) => {
   if (config.language) {
     return config.language
   }
   let l = sysLocale
   l = l ? l.toLowerCase().replace('-', '_') : defaultLang
-  return findLang(l) || defaultLang
+  return findLang(langs, l) || defaultLang
 }
 
 exports.getLang = getLang
