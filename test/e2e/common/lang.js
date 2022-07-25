6269e0141cc3cc22721ccd35a07bff5fc51d624f
ZHAO Xudong <zxdong@gmail.com>
2022-07-25 21:35:55 +0800

    [skip build] fix tests

--- test/e2e/common/lang.js
+++ test/e2e/common/lang.js
@@ -4,15 +4,11 @@
 
 const _ = require('lodash')
 
-function capitalizeFirstLetter (string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)
-}
-
 module.exports = async () => {
   const lang = require('@electerm/electerm-locales/dist/en_us.js').lang
   return prefix => {
     return (id) => {
-      return capitalizeFirstLetter(_.get(lang, `${prefix}.${id}`) || id)
+      return _.get(lang, `${prefix}.${id}`) || id
     }
   }
 }
