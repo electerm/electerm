affaee80edaf09170d88367253f3fc898c069d58
ZHAO Xudong <zxdong@gmail.com>
2022-08-03 13:13:18 +0800

    [skip build] ggg

--- test/e2e/017.1.upgrade.check.spec.js
+++ test/e2e/017.1.upgrade.check.spec.js
@@ -13,12 +13,19 @@ describe('auto upgrade check', function () {
     const electronApp = await electron.launch(appOptions)
     const client = await electronApp.firstWindow()
     extendClient(client, electronApp)
-    await client.evaluate(() => {
-      window.et.version = '0.0.0'
-    })
-
     await delay(6500)
-
+    let v = ''
+    while (v !== '0.0.0') {
+      v = await client.evaluate(() => {
+        if (window.et) {
+          window.et.version = '0.0.0'
+          return '0.0.0'
+        }
+        return ''
+      })
+      await delay(50)
+    }
+    await delay(5500)
     const sel = '.animate.upgrade-panel'
     await client.hasElem(sel)
     await electronApp.close().catch(console.log)
