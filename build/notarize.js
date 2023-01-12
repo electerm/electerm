79183fda7a48ef6377e752e290041e70bce3667c
ZHAO Xudong <zxdong@gmail.com>
2023-01-13 06:49:32 +0800

    fix build for mac os

---
+++ build/notarize.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const {
+  notarize
+} = require('electron-notarize')
+
+exports.default = async function notarizing (context) {
+  const {
+    electronPlatformName,
+    appOutDir
+  } = context
+  if (electronPlatformName !== 'darwin') {
+    return
+  }
+
+  const appName = context.packager.appInfo.productFilename
+
+  return notarize({
+    appBundleId: 'org.electerm.electerm',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS
+  })
+}
