efa0f52e604adac801b470c4c3e55289160d0917
ZHAO Xudong <zxdong@gmail.com>
2023-07-29 20:39:25 +0800

    [skip mac][skip test][skip linux] 1

--- bin/install.js
+++ bin/install.js
@@ -2,12 +2,19 @@ const { exec, echo, rm } = require('shelljs')
 const {
   resolve
 } = require('path')
+const os = require('os')
+
+const platform = os.platform()
+const isWin = platform === 'win32'
 
 const p = resolve(__dirname, '../build/iTerm2-Color-Schemes')
 const run = resolve(__dirname, 'cp-iterm')
 echo('install required modules')
 echo('install iTerm2-Color-Schemes')
 rm('-rf', p)
-
-exec(run)
+if (isWin) {
+  exec(`git clone --depth 1 https://github.com/mbadolato/iTerm2-Color-Schemes.git ${p}`)
+} else {
+  exec(run)
+}
 echo('done install required modules')
