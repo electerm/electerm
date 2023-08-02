47e68fb200a5dc5f2ec977ee823464e6244cfe16
ZHAO Xudong <zxdong@gmail.com>
2023-08-02 13:23:23 +0800

    fix proxy option

--- src/client/common/get-proxy.js
+++ src/client/common/get-proxy.js
@@ -1,7 +1,7 @@
 export default function (tab, conf) {
   if (tab.proxy && typeof tab.proxy === 'string') {
     return tab.proxy
-  } else if (conf.proxy && typeof conf.proxy === 'string') {
+  } else if (conf.enableGlobalProxy && conf.proxy && typeof conf.proxy === 'string') {
     return conf.proxy
   }
   return ''
