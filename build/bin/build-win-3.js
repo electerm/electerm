6e5fda5071df46a4f2a31a1f42f40d1d177057b3
ZHAO Xudong <zxdong@gmail.com>
2023-09-11 12:30:16 +0800

    [skip test][skip mac][skip linux] build a portale one for win (#2968)

---
+++ build/bin/build-win-3.js
@@ -0,0 +1,38 @@
+
+const replace = require('replace-in-file')
+const { rm, echo } = require('shelljs')
+const {
+  run,
+  writeSrc,
+  builder
+} = require('./build-common')
+const options = {
+  files: require('path').resolve(__dirname, '../../electron-builder.json'),
+  from: ['${productName}-${version}-${os}-${arch}.${ext}', ', "appx", "nsis"'], // eslint-disable-line
+  to: ['${productName}-${version}-${os}-${arch}-portable.${ext}', ''] // eslint-disable-line
+}
+
+function replaceRun () {
+  return new Promise((resolve, reject) => {
+    replace(options, (err) => {
+      if (err) {
+        return reject(err)
+      }
+      console.log('start build portable file')
+      resolve()
+    })
+  })
+}
+
+async function main () {
+  const pb = builder
+  echo('running build for win part 3')
+
+  echo('build portable tar.gz')
+  await replaceRun()
+  rm('-rf', 'dist')
+  writeSrc('win-x64-portable.tar.gz')
+  await run(`${pb} --win tar.gz`)
+}
+
+main()
--- src/app/common/app-props.js
+++ src/app/common/app-props.js
@@ -3,6 +3,7 @@
  */
 const { app } = require('electron')
 const constants = require('./runtime-constants')
+const installSrc = require('../lib/install-src')
 
 function getDataPath () {
   const defaultValue = {
@@ -15,6 +16,7 @@ function getDataPath () {
   const exePath = app.getPath('exe').replace('\\electerm.exe', '')
   const p = exePath + '\\' + 'electerm'
   if (
+    installSrc === 'win-x64-portable.tar.gz' ||
     require('fs').existsSync(
       p
     )
