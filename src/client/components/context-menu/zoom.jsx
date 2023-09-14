d75e4f7a1e7b3987ed9c5d64ea06676340640463
ZHAO Xudong <zxdong@gmail.com>
2023-09-14 15:25:55 +0800

    fix #2979 feat: Support input custom zoom level

--- src/client/components/context-menu/zoom.jsx
+++ src/client/components/context-menu/zoom.jsx
@@ -1,28 +1,36 @@
 import { Component } from '../common/react-subx'
-import { Button } from 'antd'
+import { Button, InputNumber, Space } from 'antd'
 
 import {
   MinusCircleOutlined,
   PlusCircleOutlined
 } from '@ant-design/icons'
 
-const { Group } = Button
-
 export default class ZoomMenu extends Component {
+  handleChange = (v) => {
+    const { store } = this.props
+    store.zoom(v / 100)
+  }
+
   render () {
     const { store } = this.props
     return (
-      <Group size='small'>
+      <Space.Compact size='small'>
         <Button onClick={() => store.zoom(0.25, true)}>
           <PlusCircleOutlined />
         </Button>
         <Button onClick={() => store.zoom(-0.25, true)}>
           <MinusCircleOutlined />
         </Button>
-        <Button onClick={() => store.zoom()}>
-          {store.config.zoom * 100}%
-        </Button>
-      </Group>
+        <InputNumber
+          value={store.config.zoom * 100}
+          onChange={this.handleChange}
+          step={1}
+          min={25}
+          max={500}
+          addonAfter='%'
+        />
+      </Space.Compact>
     )
   }
 }
