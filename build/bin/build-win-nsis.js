794d0269c7492ad3199dc14691c62f5e6ffb5238
ZHAO Xudong <zxdong@gmail.com>
2023-09-19 09:09:53 +0800

    rename file names

---
+++ build/bin/build-win-nsis.js
@@ -0,0 +1,19 @@
+
+const { rm, echo } = require('shelljs')
+const {
+  run,
+  writeSrc,
+  builder
+} = require('./build-common')
+
+async function main () {
+  const pb = builder
+  echo('running build for win part nsis installer')
+
+  echo('build nsis')
+  rm('-rf', 'dist')
+  writeSrc('win-x64-installer.exe')
+  await run(`${pb} --win nsis`)
+}
+
+main()
---
+++ build/bin/build-win-tar.js
@@ -0,0 +1,19 @@
+
+const { rm, echo } = require('shelljs')
+const {
+  run,
+  writeSrc,
+  builder
+} = require('./build-common')
+
+async function main () {
+  const pb = builder
+  echo('running build for win part 1')
+
+  echo('build tar.gz')
+  rm('-rf', 'dist')
+  writeSrc('win-x64.tar.gz')
+  await run(`${pb} --win tar.gz`)
+}
+
+main()
