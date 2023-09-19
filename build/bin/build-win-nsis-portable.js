66c1053e1739616e057cd62cd8bd1f80a15f5379
ZHAO Xudong <zxdong@gmail.com>
2023-09-19 09:30:09 +0800

    [skip test][skip mac][skip linux]

--- build/bin/build-win-nsis-portable.js
+++ build/bin/build-win-nsis-portable.js
@@ -12,7 +12,7 @@ async function main () {
 
   echo('build nsis')
   rm('-rf', 'dist')
-  writeSrc('win-x64-installer.exe')
+  writeSrc('win-x64-portable-installer.exe')
   await run(`${pb} --win nsis portable`)
 }
 
