786a9dda5e00da788dea37fb8c295ad3b898f70a
ZHAO Xudong <zxdong@gmail.com>
2023-10-16 18:36:16 +0800

    fix #3035 Fix in some cases app in mac OS can not init

--- src/app/lib/create-app.js
+++ src/app/lib/create-app.js
@@ -45,17 +45,13 @@ exports.createApp = function () {
       }
     }
   })
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
   app.on('ready', createWindow)
   app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (global.win === null) {
-      createWindow()
+      app.once('ready', createWindow)
     }
   })
-  app.createWindow = createWindow
   return app
 }
