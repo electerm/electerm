76ce47d939d5c3e234dbb20de1c410f926399b11
ZHAO Xudong <zxdong@gmail.com>
2023-11-29 09:02:14 +0800

    fix #3123 fix #3118 Fix drag window cause resize issue

--- src/app/lib/window-drag-move.js
+++ src/app/lib/window-drag-move.js
@@ -8,6 +8,8 @@ let movingInterval = null
 function windowMove (canMoving) {
   const { win } = global
   if (canMoving) {
+    win.setResizable(false)
+    const size = win.getBounds()
     const winPosition = win.getPosition()
     winStartPosition = { x: winPosition[0], y: winPosition[1] }
     mouseStartPosition = screen.getCursorScreenPoint()
@@ -20,9 +22,11 @@ function windowMove (canMoving) {
       const cursorPosition = screen.getCursorScreenPoint()
       const x = winStartPosition.x + cursorPosition.x - mouseStartPosition.x
       const y = winStartPosition.y + cursorPosition.y - mouseStartPosition.y
+      win.setSize(size.width, size.height)
       win.setPosition(x, y, true)
     }, 1)
   } else {
+    win.setResizable(true)
     clearInterval(movingInterval)
   }
 }
