c51186a5dab0f88532c41ced06cf2e25cacfa9cb
ZHAO Xudong <zxdong@gmail.com>
2023-12-26 11:40:52 +0800

    [skip test][skip mac][skip win][skip linux2] 3

--- build/bin/build-linux-deb-tar.js
+++ build/bin/build-linux-deb-tar.js
@@ -2,13 +2,11 @@ const { echo, rm } = require('shelljs')
 const {
   run,
   writeSrc,
-  builder: pb,
-  addCustomUpload
+  builder: pb
 } = require('./build-common')
 
 async function main () {
   echo('running build for linux part 1')
-  addCustomUpload()
 
   echo('build tar.gz')
   rm('-rf', 'dist')
---
+++ build/bin/custom-upload.js
@@ -0,0 +1,64 @@
+require('dotenv').config()
+const FormData = require('form-data')
+const fs = require('fs')
+const https = require('https')
+const { resolve } = require('path')
+
+async function uploadFile (filePath, fileName, conf) {
+  return new Promise((resolve, reject) => {
+    const fileStream = fs.createReadStream(filePath)
+    const formData = new FormData()
+    formData.append('file', fileStream, fileName)
+    const request = https.request({
+      method: 'post',
+      port: 443,
+      protocol: 'https:',
+      headers: formData.getHeaders(),
+      ...conf
+    })
+    formData.pipe(request)
+    request.on('response', function (res) {
+      console.log('res.statusCode', res.statusCode)
+      resolve(res.statusCode)
+    })
+    request.on('error', function (err) {
+      console.error(err)
+      reject(err)
+    })
+  })
+}
+
+const p = resolve(__dirname, '../../dist')
+const exts = [
+  'exe',
+  'dmg',
+  'gz',
+  'snap',
+  'deb',
+  'rpm',
+  'appx'
+]
+const list = fs.readdirSync(p).filter(f => {
+  const extName = f.indexOf('.') !== -1 ? f.split('.').pop() : null
+  return exts.includes(extName)
+})
+
+async function main () {
+  const {
+    CUSTOM_UPLOAD_URL
+  } = process.env
+  if (!CUSTOM_UPLOAD_URL) {
+    return console.log('CUSTOM_UPLOAD_URL is not set')
+  }
+  const host = CUSTOM_UPLOAD_URL.split('/')[2]
+  const path = '/' + CUSTOM_UPLOAD_URL.split('/').slice(3).join('/')
+  for (const n of list) {
+    const filePath = path.join(p, n)
+    await uploadFile(filePath, n, {
+      host,
+      path
+    })
+  }
+}
+
+main()
