52d65a019f12cc0a35522d6fa0d64e4d7d917bb5
ZHAO Xudong <zxdong@gmail.com>
2024-06-19 10:13:45 +0800

    save

---
+++ src/client/components/spliter/spliter.jsx
@@ -0,0 +1,27 @@
+// split session area acordding to props.store.splitTerminalSftp
+/*
+splitTerminalSftp could be belows:
+export const splitMap = {
+  c1: 'c1',
+  c2: 'c2',
+  c3: 'c3',
+  r2: 'r2',
+  r3: 'r3',
+  '2x2': '2x2',
+  '1x2r': '1x2r',
+  '1x1c': '1x2c'
+}
+export const splitMapDesc = {
+  c1: 'single',
+  c2: 'twoColumns',
+  c3: 'threeColumns',
+  r2: 'twoRows',
+  r3: 'threeRows',
+  '2x2': 'grid2x2',
+  '1x2r': 'twoRowsRight',
+  '1x1c': 'twoColumnsBottom'
+}
+
+*/
+
+
