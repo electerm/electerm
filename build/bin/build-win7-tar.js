c1deb1da5a8135f96d1f0db028798319f0984ab4
ZHAO Xudong <zxdong@gmail.com>
2024-06-19 11:24:55 +0800

    [skip mac][skip linux][skip arm-linux]

--- build/bin/build-win7-tar.js
+++ build/bin/build-win7-tar.js
@@ -1,18 +1,36 @@
 const { rm, echo } = require('shelljs')
+const replace = require('replace-in-file')
 const {
   run,
   writeSrc,
   builder
 } = require('./build-common')
+const options = {
+  files: require('path').resolve(__dirname, '../../electron-builder.json'),
+  from: ['${productName}-${version}-${os}-${arch}.${ext}', ', "appx", "nsis"'], // eslint-disable-line
+  to: ['${productName}-${version}-${os}7.${ext}', ''] // eslint-disable-line
+}
+function replaceRun () {
+  return new Promise((resolve, reject) => {
+    replace(options, (err) => {
+      if (err) {
+        return reject(err)
+      }
+      console.log('start build win7 file')
+      resolve()
+    })
+  })
+}
 
 async function main () {
   const pb = builder
-  echo('running build for win part 1')
+  echo('running build for win7')
 
   echo('build tar.gz')
+  await replaceRun()
   rm('-rf', 'dist')
-  writeSrc('win7-x64.tar.gz')
-  await run(`${pb} --win tar.gz`)
+  writeSrc('win7.tar.gz')
+  await run(`${pb} --win --ia32 tar.gz`)
 }
 
 main()
