6d551bd017fa896b7e149e23807bc4ba68068a05
ZHAO Xudong <zxdong@gmail.com>
2024-11-24 10:00:59 +0800

    Improve code

--- src/app/lib/create-app.js
+++ src/app/lib/create-app.js
@@ -45,7 +45,7 @@ exports.createApp = function () {
       }
     }
   })
-  app.on('ready', createWindow)
+  app.whenReady().then(createWindow)
   app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
