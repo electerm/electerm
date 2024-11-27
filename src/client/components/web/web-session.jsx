8ab56665fc3cfa655fc23a1faaeb5f4dfc037c4c
SpenserCai <spensercai@gmail.com>
2024-11-27 18:10:52 +0800

    add enableShowInSession

--- src/client/components/web/web-session.jsx
+++ src/client/components/web/web-session.jsx
@@ -1,52 +1,55 @@
 import { useEffect } from 'react'
-// import Link from '../common/external-link'
-
-// export default function WebSession (props) {
-//   const {
-//     tab
-//   } = props
-//   useEffect(() => {
-//     if (!tab.enableShowInSession) {
-//       tab.url && window.openLink(tab.url)
-//     }
-//   }, [])
-//   return (
-//     <div className='web-session-wrap'>
-//       {tab.enableShowInSession ? (
-//         <webview
-//           disablewebsecurity={tab.disableWebSecurity}
-//           disableblinkfeatures='true'
-//           src={tab.url}
-//           style={{ width: '100%', height: '100%' }}
-//         ></webview>
-//       ) : (
-//         <div className='pd3 aligncenter'>
-//           <h1>{tab.title}</h1>
-//           <p>{tab.description}</p>
-//           <Link to={tab.url}>{tab.url}</Link>
-//         </div>
-//       )}
-//     </div>
-//   )
-// }
+import Link from '../common/external-link'
 
 export default function WebSession (props) {
   const {
     tab
   } = props
-
   useEffect(() => {
-
+    if (!tab.enableShowInSession) {
+      tab.url && window.openLink(tab.url)
+    }
   }, [])
-
   return (
     <div className='web-session-wrap'>
-      <webview
-        disablewebsecurity='true'
-        disableblinkfeatures='true'
-        src={tab.url}
-        style={{ width: '100%', height: '100%' }}
-      />
+      {tab.enableShowInSession
+        ? (
+          <webview
+          // disablewebsecurity={tab.disableWebSecurity}
+            disablewebsecurity='true'
+            disableblinkfeatures='true'
+            src={tab.url}
+            style={{ width: '100%', height: '100%' }}
+          />
+          )
+        : (
+          <div className='pd3 aligncenter'>
+            <h1>{tab.title}</h1>
+            <p>{tab.description}</p>
+            <Link to={tab.url}>{tab.url}</Link>
+          </div>
+          )}
     </div>
   )
 }
+
+// export default function WebSession (props) {
+//   const {
+//     tab
+//   } = props
+//
+//   useEffect(() => {
+//
+//   }, [])
+//
+//   return (
+//     <div className='web-session-wrap'>
+//       <webview
+//         disablewebsecurity='true'
+//         disableblinkfeatures='true'
+//         src={tab.url}
+//         style={{ width: '100%', height: '100%' }}
+//       />
+//     </div>
+//   )
+// }
