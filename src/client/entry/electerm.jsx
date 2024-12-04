066f4f58354fe86ec3e255a384e6ac6b561e4c3b
ZHAO Xudong <zxdong@gmail.com>
2024-12-04 21:34:23 +0800

    [skip test][skip mac][skip linux][skip linux3][skip appx][skip loose][skip nsis][skip portable][skip win-tar]24

--- src/client/entry/electerm.jsx
+++ src/client/entry/electerm.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import 'antd/dist/reset.css'
 import '@xterm/xterm/css/xterm.css'
-import '../common/trzsz'
+import '../common/trzsz.js'
 import 'firacode/distr/fira_code.css'
 import Main from '../components/main/index.jsx'
 
