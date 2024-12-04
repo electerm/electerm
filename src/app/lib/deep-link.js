711d01329a2ff49435ffe4dcb91b666a89bd7628
ZHAO Xudong <zxdong@gmail.com>
2024-12-04 09:12:06 +0800

    [skip test][skip win][skip linux][skip linux3]3

--- src/app/lib/deep-link.js
+++ src/app/lib/deep-link.js
@@ -1,32 +1,36 @@
-const electronAppUniversalProtocolClient = require('electron-app-universal-protocol-client').default
 const globalState = require('./glob-state')
 const {
   isDev
 } = require('../common/runtime-constants')
 
 exports.registerDeepLink = function () {
-  electronAppUniversalProtocolClient.on(
-    'request',
-    async (url) => {
-      if (url.startsWith('electerm://')) {
-        const opts = url.slice(11)
-        const optsObj = require('querystring').parse(opts)
-        const win = globalState.get('win')
-        if (win) {
-          const port = optsObj.port ? ':' + optsObj.port : ''
-          win.webContents.send('add-tab-from-command-line', {
-            options: optsObj,
-            argv: [
-              `${optsObj.user || ''}@${optsObj.host || ''}${port}`
-            ]
-          })
+  try {
+    const electronAppUniversalProtocolClient = require('electron-app-universal-protocol-client').default
+    electronAppUniversalProtocolClient.on(
+      'request',
+      async (url) => {
+        if (url.startsWith('electerm://')) {
+          const opts = url.slice(11)
+          const optsObj = require('querystring').parse(opts)
+          const win = globalState.get('win')
+          if (win) {
+            const port = optsObj.port ? ':' + optsObj.port : ''
+            win.webContents.send('add-tab-from-command-line', {
+              options: optsObj,
+              argv: [
+                `${optsObj.user || ''}@${optsObj.host || ''}${port}`
+              ]
+            })
+          }
         }
       }
-    }
-  )
+    )
 
-  return electronAppUniversalProtocolClient.initialize({
-    protocol: 'electerm',
-    mode: isDev ? 'development' : 'production' // Make sure to use 'production' when script is executed in bundled app
-  })
+    return electronAppUniversalProtocolClient.initialize({
+      protocol: 'electerm',
+      mode: isDev ? 'development' : 'production' // Make sure to use 'production' when script is executed in bundled app
+    })
+  } catch (e) {
+    console.error(e)
+  }
 }
