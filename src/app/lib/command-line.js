5ee725a11f742485589506c140abbbf4c430efb2
ZHAO Xudong <zxdong@gmail.com>
2025-01-11 18:04:33 +0800

    Add envs to command line usage document

--- src/app/lib/command-line.js
+++ src/app/lib/command-line.js
@@ -25,24 +25,34 @@ function parseCommandLine (argv, options) {
 - electerm -l user -P 22 -i /path/to/private-key -pw password xx.com -T -t "XX Server"
 
 ### Other params examples:
-
 - server port:
 electerm -sp 30976
-
 - load and run batch operation from csv file:
 electerm -bo "/home/root/works.csv"
 
 ### other connection types
 - telnet:
 electerm -tp "telnet" -opts '{"host":"192.168.1.1","port":21","username":"root","password":"123456"}'
-
 - rdp: electerm -tp "rdp" -opts '{"host":"192.168.1.1","port":3389","username":"root","password":"123456"}'
-
 - vnc: electerm -tp "vnc" -opts '{"host":"192.168.1.1","port":3389","username":"root","password":"123456"}'
-
 - serial: electerm -tp "serial" -opts '{"port":"COM1","baudRate":115200,"dataBits":8,"stopBits":1,"parity":"none"}'
-
 - local: electerm -tp "local" -opts '{"title": "local terminal"}'
+
+### Environment variables:
+- DB_PATH:
+DB_PATH=/custom/path/to/electerm/db electerm
+
+- NO_PROXY_SERVER:
+NO_PROXY_SERVER=1 electerm
+
+- PROXY_BYPASS_LIST:
+PROXY_BYPASS_LIST="localhost, 127.0.0.1" electerm
+
+- PROXY_PAC_URL:
+PROXY_PAC_URL="http://proxy.example.com/pac" electerm
+
+- PROXY_SERVER:
+PROXY_SERVER="socks5://127.0.0.1:1080" electerm
 `)
     .option('-t, --title [Tab Name]', 'Specify the title of the new tab')
     .option('-nw, --new-window', 'Open this connection using standalone window')
