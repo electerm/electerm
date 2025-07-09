465b0f923d92563e664c5aaa24ba83e93a712fe6
ZHAO Xudong <zxdong@gmail.com>
2025-07-09 18:15:13 +0800

    #3981 Support quick command templates

--- src/client/store/quick-command.js
+++ src/client/store/quick-command.js
@@ -12,6 +12,35 @@ import generate from '../common/uid'
 import * as ls from '../common/safe-local-storage'
 import { debounce } from 'lodash-es'
 import { refs } from '../components/common/ref'
+import templates from '../components/quick-commands/templates'
+import { readClipboardAsync } from '../common/clipboard'
+
+// Function to parse templates in command string
+async function parseTemplates (cmd) {
+  if (!cmd.includes('{{')) return cmd
+
+  // Process each template from templates.js
+  for (const template of templates) {
+    const placeholder = `{{${template}}}`
+    if (cmd.includes(placeholder)) {
+      let replacement = ''
+
+      // Handle each supported template using if-else
+      if (template === 'clipboard') {
+        replacement = await readClipboardAsync()
+      } else if (template === 'time') {
+        replacement = Date.now()
+      } else if (template === 'date') {
+        replacement = new Date().toLocaleDateString()
+      }
+      // Add more conditions for any new templates as needed
+
+      cmd = cmd.replaceAll(placeholder, replacement)
+    }
+  }
+
+  return cmd
+}
 
 export default Store => {
   Store.prototype.addQuickCommand = function (
@@ -54,9 +83,13 @@ export default Store => {
           : []
         )
     for (const q of qms) {
-      const realCmd = isWin
+      let realCmd = isWin
         ? q.command.replace(/\n/g, '\n\r')
         : q.command
+
+      // Parse templates
+      realCmd = await parseTemplates(realCmd)
+
       await delay(q.delay || 100)
       runQuickCommand(realCmd, qm.inputOnly)
       store.editQuickCommand(qm.id, {
