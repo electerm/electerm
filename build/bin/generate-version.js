bacdc57245da722a3bc51c2c0a2e4fd4cb58723b
ZHAO Xudong <zxdong@gmail.com>
2025-10-05 06:28:21 +0800

    Fix build process

--- build/bin/generate-version.js
+++
@@ -1,7 +0,0 @@
-// version number creator
-const pack = require('../../package.json')
-const { version } = pack
-const { writeFileSync } = require('fs')
-const { resolve } = require('path')
-const path = resolve(__dirname, '../../work/app/version')
-writeFileSync(path, version)
