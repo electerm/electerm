965cbe7ef925a60751c7a7da99e55e26ee912930
ZHAO Xudong <zxdong@gmail.com>
2025-10-13 10:17:36 +0800

    Dowload all goes to homepage

--- src/client/common/download-mirrors.js
+++ src/client/common/download-mirrors.js
@@ -2,9 +2,5 @@ export default [
   {
     name: 'homepage',
     url: 'https://electerm.html5beta.com'
-  },
-  {
-    name: 'github',
-    url: 'https://github.com/electerm/electerm/releases'
   }
 ]
