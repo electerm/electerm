f7a4ceb4bebb1539d90868ff98c725f9b285b451
ZHAO Xudong <zxdong@gmail.com>
2025-12-09 13:46:41 +0800

    Fix export data function for web app

--- src/client/common/download.jsx
+++ src/client/common/download.jsx
@@ -5,7 +5,22 @@ import { notification } from 'antd'
 import ShowItem from '../components/common/show-item'
 import { chooseSaveDirectory } from './choose-save-folder'
 
+function downloadForBrowser (filename, text) {
+  const blob = new Blob([text], { type: 'text/plain;charset=utf-8' })
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = filename
+  document.body.appendChild(a)
+  a.click()
+  document.body.removeChild(a)
+  URL.revokeObjectURL(url)
+}
+
 export default async function download (filename, text) {
+  if (window.et.isWebApp) {
+    return downloadForBrowser(filename, text)
+  }
   const savePath = await chooseSaveDirectory()
   if (!savePath) {
     return
