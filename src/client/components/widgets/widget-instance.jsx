c3c7403c39300f3bff7858a4c85948cd042109f6
ZHAO Xudong <zxdong@gmail.com>
2025-12-21 19:35:13 +0800

    Update widget instance UI

--- src/client/components/widgets/widget-instance.jsx
+++ src/client/components/widgets/widget-instance.jsx
@@ -1,10 +1,11 @@
-import { Popconfirm } from 'antd'
-import { CloseOutlined } from '@ant-design/icons'
+import { Popconfirm, Popover } from 'antd'
+import { CloseOutlined, CopyOutlined } from '@ant-design/icons'
+import { copy } from '../../common/clipboard'
 
 const e = window.translate
 
 export default function WidgetInstance ({ item }) {
-  const { id, title } = item
+  const { id, title, serverInfo } = item
   const cls = 'item-list-unit'
   const delProps = {
     title: e('del'),
@@ -25,17 +26,51 @@ export default function WidgetInstance ({ item }) {
     cancelText: e('cancel'),
     placement: 'top'
   }
+  const handleCopy = () => {
+    if (serverInfo && serverInfo.url) {
+      copy(serverInfo.url)
+    }
+  }
+  const popoverContent = serverInfo
+    ? (
+      <div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <span>URL: {serverInfo.url}</span>
+          <CopyOutlined
+            className='pointer mg1l'
+            onClick={handleCopy}
+          />
+        </div>
+        <div>Path: {serverInfo.path}</div>
+      </div>
+      )
+    : null
+  const titleDiv = (
+    <div
+      title={title}
+      className='elli pd1y pd2x list-item-title'
+    >
+      {title}
+    </div>
+  )
   return (
     <div
       key={id}
       className={cls}
     >
-      <div
-        title={title}
-        className='elli pd1y pd2x list-item-title'
-      >
-        {title}
-      </div>
+      {
+        serverInfo
+          ? (
+            <Popover
+              content={popoverContent}
+              trigger='hover'
+              placement='top'
+            >
+              {titleDiv}
+            </Popover>
+            )
+          : titleDiv
+      }
       <Popconfirm
         {...popProps}
       >
