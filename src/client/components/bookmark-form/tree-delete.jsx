ebf8a94facb1aeff81a1881ef3c3da4d528831fb
ZHAO Xudong <zxdong@gmail.com>
2026-01-03 14:30:13 +0800

    Fix click batch edit bookmarks icon would crash app issue

--- src/client/components/bookmark-form/tree-delete.jsx
+++ src/client/components/bookmark-form/tree-delete.jsx
@@ -1,4 +1,3 @@
-import StartSessionSelect from '../setting-panel/start-session-select'
 import {
   Tree,
   Button,
@@ -6,67 +5,126 @@ import {
 } from 'antd'
 import { defaultBookmarkGroupId, settingMap } from '../../common/constants'
 import deepCopy from 'json-deep-copy'
+import { createTitleWithTag } from '../../common/create-title'
 
 const e = window.translate
 
-export default class BookmarkTreeDelete extends StartSessionSelect {
-  onExpand = (expandedKeys) => {
+function buildData (bookmarks, bookmarkGroups) {
+  const cats = bookmarkGroups
+  const tree = bookmarks
+    .reduce((p, k) => {
+      return {
+        ...p,
+        [k.id]: k
+      }
+    }, {})
+  const btree = cats
+    .reduce((p, k) => {
+      return {
+        ...p,
+        [k.id]: k
+      }
+    }, {})
+  function buildSubCats (id) {
+    const x = btree[id]
+    if (!x) {
+      return ''
+    }
+    const y = {
+      key: x.id,
+      value: x.id,
+      title: x.title
+    }
+    y.children = [
+      ...(x.bookmarkGroupIds || []).map(buildSubCats),
+      ...(x.bookmarkIds || []).map(buildLeaf)
+    ].filter(d => d)
+    if (y.children && !y.children.length) {
+      delete y.children
+    }
+    return y
+  }
+  function buildLeaf (id) {
+    const x = tree[id]
+    if (!x) {
+      return ''
+    }
+    return {
+      value: x.id,
+      key: x.id,
+      title: createTitleWithTag(x)
+    }
+  }
+  const level1 = cats.filter(d => d.level !== 2)
+    .map(d => {
+      const r = {
+        title: d.title,
+        value: d.id,
+        key: d.id,
+        children: [
+          ...(d.bookmarkGroupIds || []).map(buildSubCats),
+          ...(d.bookmarkIds || []).map(buildLeaf)
+        ].filter(d => d)
+      }
+      return r
+    }).filter(d => d)
+  return level1
+}
+
+export default function BookmarkTreeDelete (props) {
+  const { expandedKeys, checkedKeys, bookmarks, bookmarkGroups } = props
+
+  const onExpand = (expandedKeys) => {
     window.store.expandedKeys = deepCopy(expandedKeys)
   }
 
-  onCheck = (checkedKeys) => {
+  const onCheck = (checkedKeys) => {
     window.store.checkedKeys = deepCopy(checkedKeys)
   }
 
-  handleDel = () => {
+  const handleDel = () => {
     const { store } = window
-    const {
-      checkedKeys
-    } = this.props
     const arr = checkedKeys.filter(d => d !== defaultBookmarkGroupId)
     store.delItems(arr, settingMap.bookmarks)
     store.delItems(arr, settingMap.bookmarkGroups)
     store.checkedKeys = []
   }
 
-  handleCancel = () => {
+  const handleCancel = () => {
     const { store } = window
     store.bookmarkSelectMode = false
     store.checkedKeys = []
   }
 
-  render () {
-    const { expandedKeys, checkedKeys } = this.props
-    const rProps = {
-      checkable: true,
-      autoExpandParent: true,
-      onCheck: this.onCheck,
-      expandedKeys,
-      checkedKeys,
-      onExpand: this.onExpand,
-      treeData: this.buildData()
-    }
-    const len = checkedKeys.length
-    return (
-      <div>
-        <div className='pd2'>
-          <Space.Compact>
-            <Button
-              type='primary'
-              disabled={!len}
-              onClick={this.handleDel}
-            >
-              {e('delSelected')} ({len})
-            </Button>
-            <Button
-              onClick={this.handleCancel}
-            >
-              {e('cancel')}
-            </Button>
-          </Space.Compact>
-          <Tree {...rProps} />
-        </div>
-      </div>
-    )
+  const rProps = {
+    checkable: true,
+    autoExpandParent: true,
+    onCheck,
+    expandedKeys,
+    checkedKeys,
+    onExpand,
+    treeData: buildData(bookmarks, bookmarkGroups)
   }
+  const len = checkedKeys.length
+  return (
+    <div>
+      <div className='pd2'>
+        <Space.Compact>
+          <Button
+            type='primary'
+            disabled={!len}
+            onClick={handleDel}
+          >
+            {e('delSelected')} ({len})
+          </Button>
+          <Button
+            onClick={handleCancel}
+          >
+            {e('cancel')}
+          </Button>
+        </Space.Compact>
+        <Tree {...rProps} />
+      </div>
+    </div>
+  )
 }
