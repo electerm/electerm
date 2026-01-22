ad733c1fff3d4ea58dd740373c28ab333f2ff739
ZHAO Xudong <zxdong@gmail.com>
2026-01-22 09:02:26 +0800

    Improve build process

--- build/vite/conf.js
+++ build/vite/conf.js
@@ -167,8 +167,6 @@ export default defineConfig({
             }
             // Combine rest of node_modules into one chunk
             return 'vendor'
-          } else if (id.includes('batch-op/batch-op')) {
-            return 'batch-op'
           }
         },
         inlineDynamicImports: false,
