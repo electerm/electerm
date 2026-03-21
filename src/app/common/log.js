43cb5f4db4266cb0ff3bb2bc615626398769bbe0
majiayu000 <1835304752@qq.com>
2026-03-21 21:44:41 +0800

    fix: suppress debug logging in production by checking NODE_ENV
    Set console and file transport levels to 'warn' when not in
    development mode, so log.debug calls are silenced in production.
    
    Signed-off-by: majiayu000 <1835304752@qq.com>

--- src/app/common/log.js
+++ src/app/common/log.js
@@ -1,5 +1,11 @@
 const log = require('electron-log')
+const { isDev } = require('./runtime-constants')
 
 log.transports.console.format = '{h}:{i}:{s} {level} › {text}'
+
+if (!isDev) {
+  log.transports.console.level = 'warn'
+  log.transports.file.level = 'warn'
+}
 
 module.exports = exports.default = log
