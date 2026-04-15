e0e7893b8a7eb118840d1d87753a4492c3022589
ZHAO Xudong <zxdong@gmail.com>
2026-04-15 12:02:50 +0800

    #4283 Fix terminal format

--- src/app/server/session-base.js
+++ src/app/server/session-base.js
@@ -10,6 +10,12 @@ const globalState = require('./global-state')
 // const { MockBinding } = require('@serialport/binding-mock')
 // MockBinding.createPort('/dev/ROBOT', { echo: true, record: true })
 
+function createVtParser (cols = 220) {
+  const { Terminal } = require('@xterm/headless')
+  const term = new Terminal({ cols, rows: 50, allowProposedApi: true })
+  return term
+}
+
 class TerminalBase {
   constructor (initOptions, ws, isTest) {
     this.type = initOptions.termType || initOptions.type
@@ -20,6 +26,7 @@ class TerminalBase {
         logDir: initOptions.sessionLogPath,
         fileName: createLogFileName(initOptions.logName)
       })
+      this._initVtParser()
     }
     if (ws) {
       this.ws = ws
@@ -29,8 +36,23 @@ class TerminalBase {
     }
   }
 
-  cache = ''
-  prevNewLine = true
+  _initVtParser () {
+    this._vtTerm = createVtParser(this.initOptions.cols || 220)
+    this._vtLastRow = 0
+    this._vtTerm.onLineFeed(() => {
+      if (!this.sessionLogger) return
+      const buffer = this._vtTerm.buffer.active
+      const row = buffer.baseY + buffer.cursorY - 1
+      if (row < 0) return
+      const line = buffer.getLine(row)
+      if (!line) return
+      const text = line.translateToString(true)
+      const dt = this.initOptions.addTimeStampToTermLog
+        ? `[${time()}] `
+        : ''
+      this.sessionLogger.write(dt + text + '\n')
+    })
+  }
 
   toggleTerminalLogTimestamp () {
     this.initOptions.addTimeStampToTermLog = !this.initOptions.addTimeStampToTermLog
@@ -40,45 +62,24 @@ class TerminalBase {
     if (this.sessionLogger) {
       this.sessionLogger.destroy()
       delete this.sessionLogger
+      if (this._vtTerm) {
+        this._vtTerm.dispose()
+        delete this._vtTerm
+      }
     } else {
       this.sessionLogger = new SessionLog({
         logDir: this.initOptions.sessionLogPath,
         fileName: createLogFileName(this.initOptions.logName)
       })
+      this._initVtParser()
     }
-  }
-
-  parse (rawText) {
-    let result = ''
-    const len = rawText.length
-    for (let i = 0; i < len; i++) {
-      if (rawText[i] === '\b') {
-        result = result.slice(0, -1)
-      } else {
-        result += rawText[i]
-      }
-    }
-    return result
   }
 
   writeLog (data) {
-    if (!this.sessionLogger) {
+    if (!this.sessionLogger || !this._vtTerm) {
       return
     }
-    const s = data.toString()
-    if (!s.includes('\r\n')) {
-      this.cache += s
-      return
-    }
-    const p = this.parse(this.cache)
-    const dt = this.prevNewLine && this.initOptions.addTimeStampToTermLog
-      ? `[${time()}] `
-      : ''
-    const strip = require('@electerm/strip-ansi').default
-    const str = strip(dt + p + s)
-    this.sessionLogger.write(str)
-    this.cache = ''
-    this.prevNewLine = str.endsWith('\n')
+    this._vtTerm.write(data)
   }
 
   onEndConn () {
@@ -91,6 +92,10 @@ class TerminalBase {
     }
     if (this.ws) {
       delete this.ws
+    }
+    if (this._vtTerm) {
+      this._vtTerm.dispose()
+      delete this._vtTerm
     }
     if (this.server && this.server.end) {
       this.server.end()
