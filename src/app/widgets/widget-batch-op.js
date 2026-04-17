32eccf115f472bfb66bb174c1e6c8a32807b40e2
ZHAO Xudong <zxdong@gmail.com>
2026-04-17 13:35:09 +0800

    Improve batch op widget code

--- src/app/widgets/widget-batch-op.js
+++ src/app/widgets/widget-batch-op.js
@@ -6,59 +6,14 @@
 
 const uid = require('../common/uid')
 
-const workflowTemplate = `[
-  {
-    "name": "Connect SSH",
-    "action": "connect",
-    "host": "192.168.1.100",
-    "port": 22,
-    "username": "root",
-    "authType": "password",
-    "password": "your_password",
-    "enableSftp": true
-  },
-  {
-    "name": "Run Command",
-    "action": "command",
-    "command": "ls -la /tmp",
-    "wait": true
-  },
-  {
-    "name": "Upload File",
-    "action": "sftp_upload",
-    "localPath": "/local/path/file.txt",
-    "remotePath": "/remote/path/file.txt"
-  },
-  {
-    "name": "Download File",
-    "action": "sftp_download",
-    "remotePath": "/remote/path/result.log",
-    "localPath": "/local/path/result.log"
-  },
-  {
-    "name": "Run Command After Transfer",
-    "action": "command",
-    "command": "cat /remote/path/result.log",
-    "wait": true
-  }
-]`
-
 const widgetInfo = {
   name: 'Batch Operation',
   description: 'Define and execute multi-step SSH/SFTP workflows with progress tracking.',
   version: '1.0.0',
-  author: 'electerm',
   type: 'frontend',
   builtin: true,
   singleInstance: false,
-  configs: [
-    {
-      name: 'workflowJson',
-      type: 'string',
-      default: workflowTemplate,
-      description: 'Workflow definition in JSON array format'
-    }
-  ]
+  configs: []
 }
 
 function getDefaultConfig () {
