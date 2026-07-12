010df85e4f09445420775040d897d59d1bf9a828
ZHAO Xudong <zxdong@gmail.com>
2026-07-12 15:35:52 +0800

    fix: sftp transfer do not preserve modify time any more as common practice

--- src/app/server/transfer.js
+++ src/app/server/transfer.js
@@ -177,11 +177,6 @@ class Transfer {
       return th.onError(err)
     }
     this.fsize = attrs.size
-    // Store source mtime (in ms) for preserving after transfer
-    // attrs.mtime is a Date for local fs, but a number (seconds) for SFTP
-    this.srcMtime = attrs.mtime instanceof Date
-      ? attrs.mtime.getTime()
-      : attrs.mtime * 1000
     dst.open(dstPath, 'w', this.onDstOpen)
   }
 
@@ -249,22 +244,10 @@ class Transfer {
         }
       }
 
-      // Preserve source file mtime on destination after successful transfer
-      if (!err && th.srcMtime && canCloseDst) {
-        const mtimeDate = new Date(th.srcMtime)
-        dst.futimes(th.dstHandle, mtimeDate, mtimeDate, (utimesErr) => {
-          if (utimesErr) {
-            // Try utimes() as fallback for sftp servers that may not support futimes()
-            dst.utimes(dstPath, mtimeDate, mtimeDate, () => {
-              closeHandles()
-            })
-            return
-          }
-          closeHandles()
-        })
-        return
-      }
-
+      // Do not preserve source file mtime on destination after transfer.
+      // The uploaded/downloaded file should have the transfer time as both
+      // create time and modify time, which is the common practice for
+      // SFTP/FTP clients (e.g. OpenSSH sftp, FileZilla).
       closeHandles()
     }
 
